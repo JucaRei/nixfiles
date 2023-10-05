20dd45e889d4eb14cf63e500f11ce67367809b42
Reinaldo <reinaldo800@gmail.com>
2023-10-05 14:33:18 -0300

    testing

--- flake.nix
+++ flake.nix
@@ -176,7 +176,7 @@
         "juca@vortex" = libx.mkHome { hostname = "vortex"; username = "juca"; };
         # Testing
         "juca@hyperv" = libx.mkHome { hostname = "vm"; username = "juca"; desktop = "mate"; };
-        "juca@vm" = libx.mkHome { hostname = "vm"; username = "juca"; desktop = "lxqt"; };
+        "juca@vm" = libx.mkHome { hostname = "vm"; username = "juca"; };
         "juca@voidvm" = libx.mkHome { hostname = "voidvm"; username = "juca"; desktop = "i3"; };
         "juca@debianvm" = libx.mkHome { hostname = "debianvm"; username = "juca"; desktop = "i3"; };
         "juca@vm-headless" = libx.mkHome { hostname = "vm"; username = "juca"; desktop = null; };
