d45f1af2019ab0574f91f4b4c02c4ab95385fc72
Reinaldo <reinaldo800@gmail.com>
2023-10-13 14:42:08 -0300

    debug

---
+++ home-manager/_mixins/apps/config/scripts/hypr-reload.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+let
+  reload = pkgs.writeShellScriptBin "reload" ''
+    hyprctl reload &
+    pkill -SIGUSR2 waybar &
+    # reload-discord &
+    pywalfox update &
+    pkill mako && setsid mako -c /home/xenoxanite/.cache/wal/mako.conf &
+  '';
+in
+{
+  home.packages = [ reload ];
+}
