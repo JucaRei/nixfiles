ae391f0187de8635cc1a657dec55482946182809
Reinaldo <reinaldo800@gmail.com>
2023-10-13 23:16:29 -0300

    change dm

--- nixos/_mixins/desktop/hyprland.nix
+++ nixos/_mixins/desktop/hyprland.nix
@@ -14,22 +14,44 @@
     xserver = {
       xkbVariant = "nodeadkeys";
       displayManager = {
-        autoLogin = {
-          enable = false;
-          user = "${username}";
+        lightdm.enable = true;
+        lightdm.greeters.gtk = {
+          enable = true;
+          cursorTheme.name = "Yaru";
+          cursorTheme.package = pkgs.yaru-theme;
+          cursorTheme.size = 24;
+          iconTheme.name = lib.mkDefault "Yaru-magenta-dark";
+          iconTheme.package = pkgs.yaru-theme;
+          theme.name = lib.mkDefault "Yaru-magenta-dark";
+          theme.package = pkgs.yaru-theme;
+          indicators = [
+            "~session"
+            "~host"
+            "~spacer"
+            "~clock"
+            "~spacer"
+            "~a11y"
+            "~power"
+          ];
+          # https://github.com/Xubuntu/lightdm-gtk-greeter/blob/master/data/lightdm-gtk-greeter.conf
+          extraConfig = ''
+            # background = Background file to use, either an image path or a color (e.g. #772953)
+            font-name = Work Sans 12
+            xft-antialias = true
+            xft-dpi = 96
+            xft-hintstyle = slight
+            xft-rgba = rgb
+
+            active-monitor = #cursor
+            # position = x y ("50% 50%" by default)  Login window position
+            # default-user-image = Image used as default user icon, path or #icon-name
+            hide-user-image = false
+            round-user-image = false
+            highlight-logged-user = true
+            panel-position = top
+            clock-format = %a, %b %d  %H:%M
+          '';
         };
-      };
-    };
-    greetd = {
-      enable = true;
-      settings = {
-        default_session.command = ''
-          ${pkgs.greetd.tuigreet}/bin/tuigreet \
-          --time \
-          --asterisks \
-          --user-menu \
-          --cmd Hyprland
-        '';
       };
     };
   };
