0b928e6f5e4fb5b2b731fcae05ad5ef243e1d90c
Reinaldo <reinaldo800@gmail.com>
2023-10-15 23:38:06 -0300

    use gnome

--- flake.nix
+++ flake.nix
@@ -196,7 +196,7 @@
         # home-manager switch -b backup --flake $HOME/.dotfiles/nixfiles
         # home-manager switch -b backup --flake $HOME/.dotfiles/nixfiles
         # nix build .#homeConfigurations."juca@DietPi".activationPackage
-        "juca@nitro" = libx.mkHome { hostname = "nitro"; username = "juca"; desktop = "pantheon"; };
+        "juca@nitro" = libx.mkHome { hostname = "nitro"; username = "juca"; desktop = "gnome"; };
         "juca@nitrovoid" = libx.mkHome { hostname = "nitrovoid"; username = "juca"; };
         "juca@rocinante" = libx.mkHome { hostname = "rocinante"; username = "juca"; desktop = "mate"; };
         "juca@rocinante-headless" = libx.mkHome { hostname = "rocinante"; username = "juca"; desktop = null; };
@@ -247,7 +247,7 @@
         brix = libx.mkHost { hostname = "brix"; username = "juca"; };
         skull = libx.mkHost { hostname = "skull"; username = "juca"; };
         # Laptop
-        nitro = libx.mkHost { hostname = "nitro"; username = "juca"; desktop = "pantheon"; hostid = "718643c6"; };
+        nitro = libx.mkHost { hostname = "nitro"; username = "juca"; desktop = "gnome"; hostid = "718643c6"; };
         air = libx.mkHost { hostname = "air"; username = "juca"; desktop = "mate"; hostid = "718641c6"; };
         rocinante = libx.mkHost { hostname = "rocinante"; username = "juca"; desktop = "mate"; hostid = "f4173273"; };
         rocinante-headless = libx.mkHost { hostname = "rocinante"; username = "juca"; hostid = "836715d7"; };
