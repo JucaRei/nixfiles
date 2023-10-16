9a2f2284319e8c5a279bc0d8e4879b4b5035541c
reinaldo800@gmail.com <reinaldo800@gmail.com>
2023-10-16 14:27:48 -0300

    fix

--- nixos/_mixins/desktop/cinnamon.nix
+++ nixos/_mixins/desktop/cinnamon.nix
@@ -22,6 +22,16 @@
     };
     redshift = {
       enable = true;
+      settings.redshift = {
+        brightness-day = "1";
+        brightness-night = "1";
+      };
+      temperature = {
+        day = 5500;
+        night = 3000;
+      };
+      latitude = "48.864716";
+      longitude = "2.349014";
     };
     geoclue2.enable = true;
   };
