8e3443caaae501713072080c0c69f93d68fb9bdd
Reinaldo <reinaldo800@gmail.com>
2023-10-17 11:02:09 -0300

    debug

--- home-manager/_mixins/desktop/xfce4.nix
+++ home-manager/_mixins/desktop/xfce4.nix
@@ -1,8 +1,4 @@
-{ config
-, pkgs
-, lib
-, ...
-}: {
+{ config, pkgs, lib, ...}: {
   services = {
     blueman-applet.enable = true;
   };
@@ -90,89 +86,89 @@
   #  "backdrop/single-workspace-number" = 3;
   #}; # xfce4-desktop
 
-  xfce4-panel = {
-    # FIXXME: this section is not working completely; in particular: "whiskermenu"; "cpugraph"; "netload"; "eyes";
+  # xfce4-panel = {
+  #   # FIXXME: this section is not working completely; in particular: "whiskermenu"; "cpugraph"; "netload"; "eyes";
 
-    # example configurations:
-    # https://github.com/vhminh/dotfiles/blob/7b7dd80408658f0d76f8d0b518a314f5952146ec/nix/modules/desktop.nix#L62
-    # https://github.com/lobre/nix-home/blob/8117fbdb4bca887b875f622132b3b9e9c737a5bf/roles/hm/xfce/xfconf.nix#L144 -> leading slashes! :-O
+  #   # example configurations:
+  #   # https://github.com/vhminh/dotfiles/blob/7b7dd80408658f0d76f8d0b518a314f5952146ec/nix/modules/desktop.nix#L62
+  #   # https://github.com/lobre/nix-home/blob/8117fbdb4bca887b875f622132b3b9e9c737a5bf/roles/hm/xfce/xfconf.nix#L144 -> leading slashes! :-O
 
-    #  "panels" = [ 1 ];
-    #  "panels/dark-mode" = true;
-    #  "panels/panel-1/nrows" = 1; # number of rows
-    #  "panels/panel-1/mode" = 0; # Horizontal
-    #  "panels/panel-1/output-name" = "Automatic";
-    #  "panels/panel-1/span-monitors" = false;
-    #  "panels/panel-1/background-style" = 0; # None (use system style)
-    #  "panels/panel-1/icon-size" = 0; # Adjust size automatically
-    #  "panels/panel-1/size" = 24; # Row size (pixels)
-    #  "panels/panel-1/length" = 100.0;
-    #  "panels/panel-1/length-adjust" = true;
-    #  "panels/panel-1/position" = "p=6;x=0;y=0";
-    #  "panels/panel-1/enable-struts" = true;
-    ##  "panels/panel-1/position-locked" = true;
-    #  "panels/panel-1/plugin-ids" = [ 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 ];
-    # Application menu = whiskermenu
-    #  "plugins/plugin-1" = "whiskermenu";
-    # Tasklist
-    #  "plugins/plugin-2" = "tasklist";
-    #  "plugins/plugin-2/grouping" = false;
-    #  "plugins/plugin-2/show-handle" = true;
-    #  "plugins/plugin-2/show-labels" = true;
-    #  "plugins/plugin-2/flat-buttons" = false;
-    "plugins/plugin-2/include-all-monitors" = true;
-    "plugins/plugin-2/window-scrolling" = false;
-    "plugins/plugin-2/sort-order" = 1; # Group title and timestamp
-    "plugins/plugin-2/middle-click" = 0; # Nothing
-    "plugins/plugin-2/show-wireframes" = false;
-    "plugins/plugin-2/include-all-workspaces" = false;
-    # Separator
-    "plugins/plugin-3" = "separator";
-    "plugins/plugin-3/style" = 0; # transparent
-    "plugins/plugin-3/expand" = true;
-    # Workspaces
-    "plugins/plugin-4" = "pager";
-    "plugins/plugin-4/rows" = 1;
-    "plugins/plugin-4/miniature-view" = false; # show name instead of preview
-    "plugins/plugin-4/numbering" = false;
-    "plugins/plugin-4/workspace-scrolling" = false;
-    # screenshooter (if order of this item is changed → also change order of symlink below: "files in ~/.config/")
-    "plugins/plugin-5" = "screenshooter";
-    # Separator
-    "plugins/plugin-6" = "separator";
-    "plugins/plugin-6/style" = 0; # transparent
-    # Sys tray
-    "plugins/plugin-7" = "systray";
-    # CPU graph (if order of this item is changed → also change order of symlink below: "files in ~/.config/")
-    "plugins/plugin-8" = "cpugraph";
-    # Pulse audio
-    "plugins/plugin-9" = "pulseaudio";
-    "plugins/plugin-9/enable-keyboard-shortcuts" = true;
-    # Network monitor (if order of this item is changed → also change order of symlink below: "files in ~/.config/")
-    "plugins/plugin-10" = "netload";
-    # clipboard
-    "plugins/plugin-11" = "xfce4-clipman-plugin";
-    "plugins/clipman/settings/save-on-quit" = true;
-    "plugins/clipman/settings/max-texts-in-history" = 1000;
-    "plugins/clipman/settings/add-primary-clipboard" = false;
-    # Notification
-    "plugins/plugin-12" = "notification-plugin";
-    # Separator
-    "plugins/plugin-13" = "separator";
-    "plugins/plugin-13/style" = 0; # transparent
-    # Power manager
-    "plugins/plugin-14" = "power-manager-plugin";
-    # Clock
-    "plugins/plugin-15" = "clock";
-    "plugins/plugin-15/digital-layout" = 3; # Time Only
-    "plugins/plugin-15/digital-time-font" = "Sans 11";
-    "plugins/plugin-15/digital-time-format" = "%a %d %R";
-    "plugins/plugin-15/tooltip-format" = "%A %d %B %Y"; # Saturday 29 July 2023
-    "plugins/plugin-15/mode" = 2; # digital
-    "plugins/plugin-15/show-frame" = false;
-    # Eyes: where's my mouse cursor?
-    "plugins/plugin-16" = "eyes";
-  }; # xfce4-panel
+  #   #  "panels" = [ 1 ];
+  #   #  "panels/dark-mode" = true;
+  #   #  "panels/panel-1/nrows" = 1; # number of rows
+  #   #  "panels/panel-1/mode" = 0; # Horizontal
+  #   #  "panels/panel-1/output-name" = "Automatic";
+  #   #  "panels/panel-1/span-monitors" = false;
+  #   #  "panels/panel-1/background-style" = 0; # None (use system style)
+  #   #  "panels/panel-1/icon-size" = 0; # Adjust size automatically
+  #   #  "panels/panel-1/size" = 24; # Row size (pixels)
+  #   #  "panels/panel-1/length" = 100.0;
+  #   #  "panels/panel-1/length-adjust" = true;
+  #   #  "panels/panel-1/position" = "p=6;x=0;y=0";
+  #   #  "panels/panel-1/enable-struts" = true;
+  #   ##  "panels/panel-1/position-locked" = true;
+  #   #  "panels/panel-1/plugin-ids" = [ 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 ];
+  #   # Application menu = whiskermenu
+  #   #  "plugins/plugin-1" = "whiskermenu";
+  #   # Tasklist
+  #   #  "plugins/plugin-2" = "tasklist";
+  #   #  "plugins/plugin-2/grouping" = false;
+  #   #  "plugins/plugin-2/show-handle" = true;
+  #   #  "plugins/plugin-2/show-labels" = true;
+  #   #  "plugins/plugin-2/flat-buttons" = false;
+  #   "plugins/plugin-2/include-all-monitors" = true;
+  #   "plugins/plugin-2/window-scrolling" = false;
+  #   "plugins/plugin-2/sort-order" = 1; # Group title and timestamp
+  #   "plugins/plugin-2/middle-click" = 0; # Nothing
+  #   "plugins/plugin-2/show-wireframes" = false;
+  #   "plugins/plugin-2/include-all-workspaces" = false;
+  #   # Separator
+  #   "plugins/plugin-3" = "separator";
+  #   "plugins/plugin-3/style" = 0; # transparent
+  #   "plugins/plugin-3/expand" = true;
+  #   # Workspaces
+  #   "plugins/plugin-4" = "pager";
+  #   "plugins/plugin-4/rows" = 1;
+  #   "plugins/plugin-4/miniature-view" = false; # show name instead of preview
+  #   "plugins/plugin-4/numbering" = false;
+  #   "plugins/plugin-4/workspace-scrolling" = false;
+  #   # screenshooter (if order of this item is changed → also change order of symlink below: "files in ~/.config/")
+  #   "plugins/plugin-5" = "screenshooter";
+  #   # Separator
+  #   "plugins/plugin-6" = "separator";
+  #   "plugins/plugin-6/style" = 0; # transparent
+  #   # Sys tray
+  #   "plugins/plugin-7" = "systray";
+  #   # CPU graph (if order of this item is changed → also change order of symlink below: "files in ~/.config/")
+  #   "plugins/plugin-8" = "cpugraph";
+  #   # Pulse audio
+  #   "plugins/plugin-9" = "pulseaudio";
+  #   "plugins/plugin-9/enable-keyboard-shortcuts" = true;
+  #   # Network monitor (if order of this item is changed → also change order of symlink below: "files in ~/.config/")
+  #   "plugins/plugin-10" = "netload";
+  #   # clipboard
+  #   "plugins/plugin-11" = "xfce4-clipman-plugin";
+  #   "plugins/clipman/settings/save-on-quit" = true;
+  #   "plugins/clipman/settings/max-texts-in-history" = 1000;
+  #   "plugins/clipman/settings/add-primary-clipboard" = false;
+  #   # Notification
+  #   "plugins/plugin-12" = "notification-plugin";
+  #   # Separator
+  #   "plugins/plugin-13" = "separator";
+  #   "plugins/plugin-13/style" = 0; # transparent
+  #   # Power manager
+  #   "plugins/plugin-14" = "power-manager-plugin";
+  #   # Clock
+  #   "plugins/plugin-15" = "clock";
+  #   "plugins/plugin-15/digital-layout" = 3; # Time Only
+  #   "plugins/plugin-15/digital-time-font" = "Sans 11";
+  #   "plugins/plugin-15/digital-time-format" = "%a %d %R";
+  #   "plugins/plugin-15/tooltip-format" = "%A %d %B %Y"; # Saturday 29 July 2023
+  #   "plugins/plugin-15/mode" = 2; # digital
+  #   "plugins/plugin-15/show-frame" = false;
+  #   # Eyes: where's my mouse cursor?
+  #   "plugins/plugin-16" = "eyes";
+  # }; # xfce4-panel
 
   xfce4-screensaver = {
     # 2023-07-29: MUST have leading slashes
