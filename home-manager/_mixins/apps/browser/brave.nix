aab1db08a1dfb7cdbe9a91b5824d4d4e8f612076
Reinaldo <reinaldo800@gmail.com>
2023-10-19 17:39:30 -0300

    check

--- home-manager/_mixins/apps/browser/brave.nix
+++ home-manager/_mixins/apps/browser/brave.nix
@@ -1,11 +1,13 @@
-{ pkgs, lib, params, ... }:
+{ lib
+, params
+, pkgs
+, ...
+}:
 let
   ifDefault = lib.mkIf (builtins.elem params.browser [ "brave" "brave-browser" ]);
 in
 {
-  home.packages = with pkgs.unstable;[
-    brave
-  ];
+  home.packages = [ pkgs.brave ];
 
   xdg.mime.enable = ifDefault true;
   xdg.mimeApps.enable = ifDefault true;
