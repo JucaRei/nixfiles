77cd3d5d823615c51f124ec79b95cdc13aa16860
Reinaldo <reinaldo800@gmail.com>
2023-10-19 17:22:43 -0300

    lighter

--- home-manager/hosts/vm.nix
+++ home-manager/hosts/vm.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }: {
   imports = [
     ../_mixins/apps/text-editor/vscodium.nix
+    ../_mixins/apps/browser/brave.nix
   ];
 
   home.packages = with pkgs; [
-    thorium
+    # thorium
   ];
 }
