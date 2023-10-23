364072011a9f9606499133a5011d7be772f69839
Reinaldo <reinaldo800@gmail.com>
2023-10-23 11:38:28 -0300

    fix

--- nixos/hosts/air/default.nix
+++ nixos/hosts/air/default.nix
@@ -59,7 +59,8 @@
       "vm.dirty_ratio" = 50;
       "dev.i915.perf_stream_paranoid" = 0;
     };
-    kernelPackages = pkgs.linuxPackages_lqx;
+    # kernelPackages = pkgs.linuxPackages_lqx;
+    kernelPackages = pkgs.linuxPackages_5_10;
     supportedFilesystems = [ "btrfs" ];
 
     plymouth = {
