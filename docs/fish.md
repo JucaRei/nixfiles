6085450243b61bb50baba86af6756c22af345d5c
Reinaldo <reinaldo800@gmail.com>
2023-10-24 16:31:53 -0300

    cleanup

--- docs/fish.md
+++
@@ -1,7 +0,0 @@
-### Set env variable forever
-
-Using universal variables, the env variable will be preserved even after restarts.
-
-```shell
-set -Ux GITHUB_TOKEN some-secret
-```
--- shell.nix
+++ shell.nix
@@ -1,7 +1,13 @@
 # Shell for bootstrapping flake-enabled nix and home-manager
 # Enter it through 'nix develop' or (legacy) 'nix-shell'
 
-{ pkgs ? (import ./nixpkgs.nix) { } }: {
+{ pkgs ? (import ./nixpkgs.nix) { } }:
+with pkgs; let
+  nixBin = writeShellScriptBin "nix" ''
+    ${nixVersions.stable}/bin/nix --option experimental-features "nix-command flakes" "$@"
+  '';
+in
+{
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
@@ -18,6 +24,8 @@
     ];
     shellHook = ''
       # alias ssh=dbclient
+      export FLAKE="$(pwd)"
+      export PATH="$FLAKE/bin:${nixBin}/bin:$PATH"
       echo "
       . _____   _           _
       |  ____| | |         | |
