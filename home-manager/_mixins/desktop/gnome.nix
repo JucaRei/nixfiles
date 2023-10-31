e183e2cebf5ea42c02e55bb6817f2cded8e25efe
Reinaldo <reinaldo800@gmail.com>
2023-10-31 12:41:19 -0300

    test

--- home-manager/_mixins/desktop/gnome.nix
+++ home-manager/_mixins/desktop/gnome.nix
@@ -11,20 +11,22 @@ with lib.hm.gvariant;
 
   home = {
     packages = with pkgs; [
-      gnomeExtensions.tray-icons-reloaded
       gnomeExtensions.blur-my-shell
-      gnomeExtensions.removable-drive-menu
-      gnomeExtensions.dash-to-panel
-      gnomeExtensions.battery-indicator-upower
       gnomeExtensions.just-perfection
-      gnomeExtensions.caffeine
       gnomeExtensions.clipboard-indicator
-      gnomeExtensions.workspace-indicator-2
-      gnomeExtensions.bluetooth-quick-connect
+      gnomeExtensions.user-themes
+      gnomeExtensions.aylurs-widgets
+      # gnomeExtensions.tray-icons-reloaded
+      # gnomeExtensions.removable-drive-menu
+      # gnomeExtensions.dash-to-panel
+      # gnomeExtensions.battery-indicator-upower
+      # gnomeExtensions.caffeine
+      # gnomeExtensions.workspace-indicator-2
+      # gnomeExtensions.bluetooth-quick-connect
       gnomeExtensions.gsconnect # kdeconnect enabled in default.nix
-      gnomeExtensions.pip-on-top
-      gnomeExtensions.pop-shell
-      gnomeExtensions.forge
+      # gnomeExtensions.pip-on-top
+      # gnomeExtensions.pop-shell
+      # gnomeExtensions.forge
       # gnomeExtensions.fullscreen-avoider
       # gnomeExtensions.dash-to-dock
 
