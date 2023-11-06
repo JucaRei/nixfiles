bb0c4f8699ed29b92eeae16e2fe4816edb481e92
Reinaldo <reinaldo800@gmail.com>
2023-11-06 15:44:42 -0300

    remove extension from vscodium

--- home-manager/hosts/vm.nix
+++ home-manager/hosts/vm.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }: {
   imports = [
-    # ../_mixins/apps/text-editor/vscodium.nix
-    ../_mixins/apps/text-editor/vscode.nix
+    ../_mixins/apps/text-editor/vscodium.nix
+    # ../_mixins/apps/text-editor/vscode.nix
     # ../_mixins/apps/text-editor/sublime.nix
-    ../_mixins/apps/browser/brave.nix
-    ../_mixins/apps/terminal/alacritty.nix
+    # ../_mixins/apps/browser/brave.nix
+    ../_mixins/apps/browser/firefox.nix
+    # ../_mixins/apps/terminal/alacritty.nix
   ];
 
   home.packages = with pkgs; [
