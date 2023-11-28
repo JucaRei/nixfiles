5613c2088703a13e66523a9cbf3745a12f4b4809
Reinaldo <reinaldo800@gmail.com>
2023-11-28 17:00:22 -0300

    fix

--- nixos/_mixins/desktop/hyprland.nix
+++ nixos/_mixins/desktop/hyprland.nix
@@ -129,7 +129,7 @@
     enable = true;
     # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     # package = (inputs.hyprland.packages.${pkgs.system}.hyprland.override { legacyRenderer = true; });
-    package = pkgs.hyprland.overrideAttrs (_: {
+    package = pkgs.unstable.hyprland.overrideAttrs (_: {
       mesonFlags = [ "-DLEGACY_RENDERER:STRING=true" ];
     });
     xwayland.enable = true;
