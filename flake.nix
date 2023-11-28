9b13d08d4a0b0ff78cf1bd9c58e378803971fc4a
Reinaldo <reinaldo800@gmail.com>
2023-11-28 12:51:28 -0300

    remove cachix for now

--- flake.nix
+++ flake.nix
@@ -1,18 +1,18 @@
 {
   description = "Juca's NixOS and Home Manager Configuration";
 
-  nixConfig = {
-    extra-substituters = [
-      # "https://nix-community.cachix.org"
-      "https://juca-nixfiles.cachix.org"
-      "https://hyprland.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "juca-nixfiles.cachix.org-1:HN1wk6GxLI1ZPr3bN2RNa+a4jXwLGUPJG6zXKqDZ/Kc="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-  };
+  # nixConfig = {
+  #   extra-substituters = [
+  #     "https://nix-community.cachix.org"
+  #     "https://juca-nixfiles.cachix.org"
+  #     "https://hyprland.cachix.org"
+  #   ];
+  #   extra-trusted-public-keys = [
+  #     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  #     "juca-nixfiles.cachix.org-1:HN1wk6GxLI1ZPr3bN2RNa+a4jXwLGUPJG6zXKqDZ/Kc="
+  #     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+  #   ];
+  # };
 
   inputs = {
     # nixpkgs-prev.url = "github:nixos/nixpkgs/nixos-22.11";
--- home-manager/default.nix
+++ home-manager/default.nix
@@ -114,16 +114,16 @@ in
       # Allow to run nix
       allowed-users = [ "${username}" "nixbld" "wheel" "juca-nixfiles" ];
 
-      substituters = [
-        # "https://nix-community.cachix.org"
-        "https://hyprland.cachix.org"
-        "https://juca-nixfiles.cachix.org"
-      ];
-      trusted-public-keys = [
-        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "juca-nixfiles.cachix.org-1:HN1wk6GxLI1ZPr3bN2RNa+a4jXwLGUPJG6zXKqDZ/Kc="
-      ];
+      # substituters = [
+      #   "https://nix-community.cachix.org"
+      #   "https://hyprland.cachix.org"
+      #   "https://juca-nixfiles.cachix.org"
+      # ];
+      # trusted-public-keys = [
+      #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      #   "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      #   "juca-nixfiles.cachix.org-1:HN1wk6GxLI1ZPr3bN2RNa+a4jXwLGUPJG6zXKqDZ/Kc="
+      # ];
     };
     extraOptions = ''
       keep-outputs          = true
