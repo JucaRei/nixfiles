cdc787c74f49ac17cd866ea36e62540b12d73340
Reinaldo <reinaldo800@gmail.com>
2023-12-04 14:00:41 -0300

    test

--- home-manager/hosts/debianvm.nix
+++ home-manager/hosts/debianvm.nix
@@ -1,8 +1,25 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+let
+  # ...
+  nixgl = import <nixgl> {} ;
+  nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+    mkdir $out
+    ln -s ${pkg}/* $out
+    rm $out/bin
+    mkdir $out/bin
+    for bin in ${pkg}/bin/*; do
+      wrapped_bin=$out/bin/$(basename $bin)
+      echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
+      chmod +x $wrapped_bin
+    done
+  ''; 
+  in
+{
   home = {
     packages = with pkgs; [
       st
+    ] ++ (nixgl.auto.nixGLDefault) (with nixGLWrap pkgs;[
       firefox
-    ];
+    ]);
   };
 }
