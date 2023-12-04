07ca3a9f4f993ef8461df5e6c47c1a1680564e6e
Reinaldo <reinaldo800@gmail.com>
2023-12-04 15:09:55 -0300

    a

--- lib/nixgl.nix
+++ lib/nixgl.nix
@@ -2,11 +2,52 @@
 
 let
 
-  
+  # nixGLMesaWrap = pkg:
+  #   pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+  #     mkdir $out
+  #     ln -s ${pkg}/* $out
+  #     rm $out/bin
+  #     mkdir $out/bin
+  #     for bin in ${pkg}/bin/*; do
+  #      wrapped_bin=$out/bin/$(basename $bin)
+  #      echo "exec ${lib.getExe pkgs.nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
+  #      chmod +x $wrapped_bin
+  #     done
+  #   '';
+
+  # nixGLVulkanWrap = pkg:
+  #   pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+  #     mkdir $out
+  #     ln -s ${pkg}/* $out
+  #     rm $out/bin
+  #     mkdir $out/bin
+  #     for bin in ${pkg}/bin/*; do
+  #      wrapped_bin=$out/bin/$(basename $bin)
+  #      echo "exec ${
+  #        lib.getExe pkgs.nixgl.nixVulkanIntel
+  #      } $bin \$@" > $wrapped_bin
+  #      chmod +x $wrapped_bin
+  #     done
+  #   '';
+
+  # nixGLVulkanMesaWrap = pkg:
+  #   pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+  #     mkdir $out
+  #     ln -s ${pkg}/* $out
+  #     rm $out/bin
+  #     mkdir $out/bin
+  #     for bin in ${pkg}/bin/*; do
+  #      wrapped_bin=$out/bin/$(basename $bin)
+  #      echo "${lib.getExe pkgs.nixgl.nixGLIntel} ${
+  #        lib.getExe pkgs.nixgl.nixVulkanIntel
+  #      } $bin \$@" > $wrapped_bin
+  #      chmod +x $wrapped_bin
+  #     done
+  #   '';
 
 in 
 {
-  nixGLMesaWrap = nixGLMesaWrap;
+  # nixGLMesaWrap = nixGLMesaWrap;
   fonts.fontconfig.enable = true;
   targets.genericLinux.enable = true;
   home = {
