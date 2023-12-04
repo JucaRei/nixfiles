91056684e6b2eae3a17f1f5a1d63817d1696a1a4
Reinaldo <reinaldo800@gmail.com>
2023-12-04 14:23:22 -0300

    test

--- home-manager/hosts/debianvm.nix
+++ home-manager/hosts/debianvm.nix
@@ -9,14 +9,13 @@ let
     mkdir $out/bin
     for bin in ${pkg}/bin/*; do
       wrapped_bin=$out/bin/$(basename $bin)
-      echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \$@" > $wrapped_bin
+      echo "exec ${lib.getExe nixgl.nixGLDefault} $bin \$@" > $wrapped_bin
       chmod +x $wrapped_bin
     done
   ''; 
   in
 {
-   home.packages = [
-    nixgl.auto.nixGLDefault
+  home.packages = [
     (nixGLWrap pkgs.st)
     (nixGLWrap pkgs.firefox)
   ] ++ (with pkgs; [
