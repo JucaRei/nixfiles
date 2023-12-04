f03de05eb6d91427bb4701ef97a60e8302c5bbae
Reinaldo <reinaldo800@gmail.com>
2023-12-04 15:05:54 -0300

    a

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,4 +1,4 @@
-{ config, desktop, inputs, lib, outputs, pkgs, modulesPath, stateVersion, username, hostname, ... }:
+{ config, desktop, inputs, lib, outputs, pkgs, modulesPath, stateVersion, username, hostname, nixgl, ... }:
 let
   inherit (pkgs.stdenv) isDarwin;
 in
