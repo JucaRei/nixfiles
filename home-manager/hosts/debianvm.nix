9573173d4f3dfa5938eb5f701070b96fe0bc3602
Reinaldo <reinaldo800@gmail.com>
2023-12-04 14:41:14 -0300

    tes

--- home-manager/hosts/debianvm.nix
+++ home-manager/hosts/debianvm.nix
@@ -1,25 +1,32 @@
 { pkgs, lib, nixgl, ... }:
 let
   # ...
-  imports =[ nixgl ];
-  nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-    mkdir $out
-    ln -s ${pkg}/* $out
-    rm $out/bin
-    mkdir $out/bin
-    for bin in ${pkg}/bin/*; do
-      wrapped_bin=$out/bin/$(basename $bin)
-      echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \"\$@\"" > $wrapped_bin
-      chmod +x $wrapped_bin
-    done
-  ''; 
+  # imports =[ nixgl ];
+  # nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
+  #   mkdir $out
+  #   ln -s ${pkg}/* $out
+  #   rm $out/bin
+  #   mkdir $out/bin
+  #   for bin in ${pkg}/bin/*; do
+  #     wrapped_bin=$out/bin/$(basename $bin)
+  #     echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \"\$@\"" > $wrapped_bin
+  #     chmod +x $wrapped_bin
+  #   done
+  # ''; 
   in
 {
-  home.packages = [
-    nixgl.nixGLDefault
-    (nixGLWrap pkgs.st)
-    (nixGLWrap pkgs.firefox)
-  ] ++ (with pkgs; [
-    inxi
-  ]);
+#   home.packages = [
+#     nixgl.nixGLDefault
+#     (nixGLWrap pkgs.st)
+#     (nixGLWrap pkgs.firefox)
+#   ] ++ (with pkgs; [
+#     inxi
+#   ]);
+
+  home = {
+    packages = with pkgs; [
+      firefox
+      st
+    ];
+  };
 }
