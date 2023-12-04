599af62afe2dd9fc43d4aa389073d2336d4f23c1
Reinaldo <reinaldo800@gmail.com>
2023-12-04 15:09:29 -0300

    test

--- home-manager/hosts/debianvm.nix
+++ home-manager/hosts/debianvm.nix
@@ -1,28 +1,57 @@
 { pkgs, lib, nixgl, config, specialArgs, ... }:
 let
-  # ...
-  # imports =[ nixgl ];
-  # nixGLWrap = pkg: pkgs.runCommand "${pkg.name}-nixgl-wrapper" {} ''
-  #   mkdir $out
-  #   ln -s ${pkg}/* $out
-  #   rm $out/bin
-  #   mkdir $out/bin
-  #   for bin in ${pkg}/bin/*; do
-  #     wrapped_bin=$out/bin/$(basename $bin)
-  #     echo "exec ${lib.getExe nixgl.auto.nixGLDefault} $bin \"\$@\"" > $wrapped_bin
-  #     chmod +x $wrapped_bin
-  #   done
-  # ''; 
+  nixGLMesaWrap = pkg:
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+       wrapped_bin=$out/bin/$(basename $bin)
+       echo "exec ${lib.getExe pkgs.nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
+       chmod +x $wrapped_bin
+      done
+    '';
+
+  nixGLVulkanWrap = pkg:
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+       wrapped_bin=$out/bin/$(basename $bin)
+       echo "exec ${
+         lib.getExe pkgs.nixgl.nixVulkanIntel
+       } $bin \$@" > $wrapped_bin
+       chmod +x $wrapped_bin
+      done
+    '';
+
+  nixGLVulkanMesaWrap = pkg:
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+       wrapped_bin=$out/bin/$(basename $bin)
+       echo "${lib.getExe pkgs.nixgl.nixGLIntel} ${
+         lib.getExe pkgs.nixgl.nixVulkanIntel
+       } $bin \$@" > $wrapped_bin
+       chmod +x $wrapped_bin
+      done
+    '';
   
   in
 {
-  # home.packages = [
-  #   nixgl.auto.nixGLDefault
-  #   (nixGLVulkanMesaWrap pkgs.st)
-  #   (nixGLVulkanMesaWrap pkgs.firefox)
-  # ] ++ (with pkgs; [
-  #   inxi
-  # ]);
+  home.packages = [
+    nixgl.auto.nixGLDefault
+    (nixGLVulkanMesaWrap pkgs.st)
+    (nixGLVulkanMesaWrap pkgs.firefox)
+  ] ++ (with pkgs; [
+    inxi
+  ]);
 
   home = {
     packages = with pkgs; [
--- lib/nixgl.nix
+++ lib/nixgl.nix
@@ -2,48 +2,7 @@
 
 let
 
-  nixGLMesaWrap = pkg:
-    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      for bin in ${pkg}/bin/*; do
-       wrapped_bin=$out/bin/$(basename $bin)
-       echo "exec ${lib.getExe pkgs.nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
-       chmod +x $wrapped_bin
-      done
-    '';
-
-  nixGLVulkanWrap = pkg:
-    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      for bin in ${pkg}/bin/*; do
-       wrapped_bin=$out/bin/$(basename $bin)
-       echo "exec ${
-         lib.getExe pkgs.nixgl.nixVulkanIntel
-       } $bin \$@" > $wrapped_bin
-       chmod +x $wrapped_bin
-      done
-    '';
-
-  nixGLVulkanMesaWrap = pkg:
-    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
-      mkdir $out
-      ln -s ${pkg}/* $out
-      rm $out/bin
-      mkdir $out/bin
-      for bin in ${pkg}/bin/*; do
-       wrapped_bin=$out/bin/$(basename $bin)
-       echo "${lib.getExe pkgs.nixgl.nixGLIntel} ${
-         lib.getExe pkgs.nixgl.nixVulkanIntel
-       } $bin \$@" > $wrapped_bin
-       chmod +x $wrapped_bin
-      done
-    '';
+  
 
 in 
 {
@@ -54,7 +13,7 @@ in
     packages =
       [
         # (import nixgl { inherit pkgs; }).nixGLIntel # OpenGL for GUI apps
-        # (import nixgl { inherit pkgs; }).nixVulkanIntel # OpenGL for GUI apps
+        (import nixgl { inherit pkgs; }).nixVulkanIntel # OpenGL for GUI apps
         # pkgs.nixGLVulkanMesaWrap
         # nixGLVulkanWrap
         # nixGLMesaWrap
