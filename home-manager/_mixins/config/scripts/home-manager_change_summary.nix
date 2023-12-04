e9814287669210ba065149a347fded90b52a47f2
Reinaldo <reinaldo800@gmail.com>
2023-12-04 19:43:19 -0300

    fix

--- home-manager/_mixins/config/scripts/home-manager_change_summary.nix
+++ home-manager/_mixins/config/scripts/home-manager_change_summary.nix
@@ -2,7 +2,7 @@
 let
   home-manager_change_summary = pkgs.writeShellScriptBin "home-manager_change_summary" ''
       BUILDS=$(${pkgs.coreutils-full}/bin/ls -d1v ''${XDG_STATE_HOME}/nix/profiles/home-manager-*-link | tail -n 2)
-    sudo ${pkgs.nvd}/bin/nvd diff ''${BUILDS}
+    ${pkgs.nvd}/bin/nvd diff ''${BUILDS}
   '';
 in
 {
