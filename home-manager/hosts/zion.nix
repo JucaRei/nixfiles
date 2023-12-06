69cbca45539fcb6a1c5e194e70af3b1eec87e88c
Reinaldo <reinaldo800@gmail.com>
2023-12-06 14:53:13 -0300

    test

--- home-manager/hosts/zion.nix
+++ home-manager/hosts/zion.nix
@@ -1,8 +1,61 @@
-{ pkgs, ... }: {
-  home = {
-    packages = with pkgs; [
-      st
-      firefox
+{ pkgs, lib, ... }:
+let
+  # For nixgl without having to always use nixgl.pkgs application before use it
+  nixGLMesaWrap = pkg:
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+        wrapped_bin=$out/bin/$(basename $bin)
+        echo "exec ${lib.getExe pkgs.nixgl.nixGLIntel} $bin \$@" > $wrapped_bin
+        chmod +x $wrapped_bin
+      done
+    '';
+
+  nixGLVulkanWrap = pkg:
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+        wrapped_bin=$out/bin/$(basename $bin)
+        echo "exec ${
+          lib.getExe pkgs.nixgl.nixVulkanIntel
+        } $bin \$@" > $wrapped_bin
+        chmod +x $wrapped_bin
+      done
+    '';
+
+  nixGLVulkanMesaWrap = pkg:
+    pkgs.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+        wrapped_bin=$out/bin/$(basename $bin)
+        echo "${lib.getExe pkgs.nixgl.nixGLIntel} ${
+          lib.getExe pkgs.nixgl.nixVulkanIntel
+        } $bin \$@" > $wrapped_bin
+        chmod +x $wrapped_bin
+      done
+    '';
+
+in
+{
+  config = {
+    imports = [
+      (nixGLVulkanMesaWrap ../_mixins/apps/text-editor/vscode.nix)
     ];
+    home = {
+      packages = [
+        (nixGLVulkanMesaWrap pkgs.thorium)
+      ] ++ (with pkgs;[
+        st
+      ]);
+    };
   };
 }
