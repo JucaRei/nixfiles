253b6c76d1babbbd0cfe44573e1a512dbe111d04
Reinaldo <reinaldo800@gmail.com>
2023-12-06 14:01:43 -0300

    fix

--- home-manager/_mixins/desktop/bspwm/themes/everforest/default.nix
+++ home-manager/_mixins/desktop/bspwm/themes/everforest/default.nix
@@ -16,9 +16,9 @@
     };
     packages = with pkgs; [
       pulseaudio-control
-      nautilus
+      gnome.nautilus
+      gnome.sushi
       nautilus-open-any-terminal
-      nautilus-python
     ];
   };
 
@@ -28,6 +28,19 @@
       "sleep 2s;polybar -q main"
       "xsetroot -cursor_name left_ptr"
       "dunst -config $HOME/.config/dunst/dunstrc"
+      "$HOME/.screenlayout/vm.sh"
     ];
+  };
+  services = {
+    sxhkd = {
+      keybindings = {
+        # Selected programs
+        "super + Return" = "st"; # terminal emulator
+        "super + @space" = "rofi -show drun -show-icons"; # program launcher
+        "super + Escape" = "pkill -USR1 -x sxhkd"; # make sxhkd reload its configuration files
+        "super + e" = "nemo";
+        "super + b" = "thorium";
+      };
+    };
   };
 }
