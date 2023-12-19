e479918e04317e26d3040b5f2a0796ac6030745c
Reinaldo <reinaldo800@gmail.com>
2023-12-18 22:21:45 -0300

    fix

--- home-manager/_mixins/desktop/bspwm/themes/everforest/sxhkd_us-mac.nix
+++ home-manager/_mixins/desktop/bspwm/themes/everforest/sxhkd_us-mac.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs,...}: {
   services = {
     sxhkd = {
       enable = true;
@@ -92,12 +92,14 @@ _: {
         "XF86AudioRaiseVolume" = "pactl -- set-sink-volume 0 +2%"; # Raise volume
         "XF86AudioLowerVolume" = "pactl -- set-sink-volume 0 -2%"; # Lower volume
         "XF86AudioMicMute" = "pactl set-source-mute 1 toggle"; # Toggle mute mic audio
-        "XF86KbdBrightnessUp" = "kbdlight up 2";
-        "XF86KbdBrightnessDown" = "kbdlight down 2";
+        "XF86KbdBrightnessUp" = "${pkgs.kbdlight} up 2";
+        "XF86KbdBrightnessDown" = "${pkgs.kbdlight} down 2";
         #"XF86MonBrightnessUp" = "xbacklight -inc 2"; # Brightness down
         #"XF86MonBrightnessDown" = "xbacklight -dec 2"; # Brightness down
-        "XF86MonBrightnessUp" = "light -A 2"; # Brightness up
-        "XF86MonBrightnessDown" = "light -U 2"; # Brightness down
+        # "XF86MonBrightnessUp" = "light -A 2"; # Brightness up
+        # "XF86MonBrightnessDown" = "light -U 2"; # Brightness down
+        "XF86MonBrightnessUp" = "${pkgs.brillo} -A 2"; # Brightness up
+        "XF86MonBrightnessDown" = "${pkgs.brillo} -U 2"; # Brightness down
         # "XF86MonBrightnessDown" = "light -U  5"; # Brightness down
         # "XF86MonBrightnessUp" = "light -A 5";
 
