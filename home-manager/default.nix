efdce270d685ef7642e985f5480469103c39de8c
Reinaldo <reinaldo800@gmail.com>
2024-02-16 14:58:51 -0300

    testing

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -27,7 +27,6 @@ in {
 
       # You can also split up your configuration and import pieces of it here:
       ./_mixins
-      ./_mixins/dev
     ]
     # ++ lib.optional (builtins.isString desktop) ./_mixins/desktop
     # ++ lib.optional (builtins.isPath (./. + "/_mixins/users/${username}")) ./_mixins/users/${username}
