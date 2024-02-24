5ab8efaf10e515b24a3b9e72680133ff3895ca5f
Reinaldo <reinaldo800@gmail.com>
2024-02-23 21:57:26 -0300

    fix

--- home-manager/hosts/anubis.nix
+++ home-manager/hosts/anubis.nix
@@ -4,7 +4,7 @@
     ../_mixins/apps/text-editor/vscode.nix
     # ../_mixins/apps/text-editor/vscodium.nix
     ../_mixins/apps/terminal/alacritty.nix
-    ../_mixins/apps/browser/firefox.nix
+    ../_mixins/apps/browser/firefox/librewolf.nix
     ../_mixins/apps/video/mpv.nix
     ../_mixins/non-nixos
     # ../_mixins/apps/tools/zathura.nix
