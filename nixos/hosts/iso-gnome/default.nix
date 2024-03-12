228d80ae1d7458687d8be4d1329903b46c95e1cf
Reinaldo <reinaldo800@gmail.com>
2024-03-12 19:09:09 +0000

    a

--- nixos/hosts/iso-gnome/default.nix
+++ nixos/hosts/iso-gnome/default.nix
@@ -1,1 +1,12 @@
-{ }
+{ lib, ... }: {
+  imports = [
+    ../../_mixins/hardware/bluetooth
+    ../../_mixins/hardware/sound/pipewire.nix
+    ../../_mixins/apps/text-editor/vscode.nix
+    ../../_mixins/virtualization/podman.nix
+  ];
+
+  boot.kernelParams = [ "nomodeset" ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
