dc386d72bdb3bc02de59c74106dfd84ce6bf79dd
Reinaldo <reinaldo800@gmail.com>
2024-03-25 15:14:57 -0300

    a

--- home-manager/_mixins/apps/text-editor/vscode/vscode-config.nix
+++ home-manager/_mixins/apps/text-editor/vscode/vscode-config.nix
@@ -1,9 +1,9 @@
 { username, config, ... }@args: {
-  home = args {
+  home = {
     # packages = pkgs.unstable.vscode-fhs;
     file.".config/Code/User/settings.json" = {
       # source = ../../../config/vscode/settings.json;
       source = ../../../config/vscode/settings-test.nix;
-    };
+    } args;
   };
 }
