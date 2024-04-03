42d00d6060501e631e1f6a81b23c1ffbd2df6084
Reinaldo <reinaldo800@gmail.com>
2024-04-03 14:04:33 -0300

    fix

--- nixos/_mixins/config/scripts/build-host.nix
+++ nixos/_mixins/config/scripts/build-host.nix
@@ -5,7 +5,7 @@ pkgs.writeScriptBin "build-host" ''
 
   if [ -e $HOME/.dotfiles/nixfiles ]; then
     all_cores=$(nproc)
-    build_cores=$(printf "%.0f" $(echo "$all_cores * 0.75" | ${pkgs.bc}/bin/bc))
+    build_cores=$(${pkgs.uutils-coreutils-noprefix}/bin/printf "%.0f" $(echo "$all_cores * 0.75" | ${pkgs.bc}/bin/bc))
     pushd $HOME/.dotfiles/nixfiles 2>&1 > /dev/null
     echo "Building NixOS with $build_cores cores"
     nixos-rebuild build --flake .# -L --impure --cores $build_cores
