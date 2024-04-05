93e4c59ba9830692a5b0a9ce42c2931b1400074c
Reinaldo <reinaldo800@gmail.com>
2024-04-05 15:09:38 -0300

    clear

--- home-manager/_mixins/desktop/bspwm/default.nix
+++ home-manager/_mixins/desktop/bspwm/default.nix
@@ -1,58 +1,21 @@
-{ config, lib, pkgs, username, hostname, ... }:
-with lib.hm.gvariant; {
-  imports = [
-    ### Import selected theme
-    # ./themes/default
-    ./themes/everforest
-    ./xsession.nix
-  ];
+{ pkgs, config, lib, ... }:
+let
+  nixgl = import ../../../../lib/nixGL.nix { inherit config pkgs; };
+in
+{
 
-  config = {
-    modules.bspwm-xs.enable = true;
-    home = {
-      packages = with pkgs; [
-        # Default packages for ALL
-        pamixer # Pulseaudio command line mixer
-        gnome.gvfs # Virtual Filesystem support library
-        cifs-utils # Tools for managing Linux CIFS client filesystems
-        lm_sensors # Tools for reading hardware sensors
-        lxappearance-gtk2 # Lightweight program for configuring the theme and fonts of gtk applications
-        imagemagick
-        xclip
-        lsof
-        xdo
-        wmctrl
-        mate.mate-polkit
-        brillo
-        kbdlight
-        acpi
-      ];
+  home = {
+    packages = with pkgs; [
+      ### Window
+      bpswm
+      wmname
+      sxhkd
+      dunst
+      betterlockscreen
 
-      sessionVariables = {
-        GLFW_IM_MODULE = "ibus";
-        LIBPROC_HIDE_KERNEL = "true"; # prevent display kernel threads in top
-        QT_QPA_PLATFORMTHEME = "gtk3";
-        "TERM" = "xterm";
-        GIO_EXTRA_MODULES = "${pkgs.gnome.gvfs}/lib/gio/modules";
-        "_JAVA_AWT_WM_NONREPARENTING" = "1";
-      };
-      sessionPath = [ "$HOME/.local/bin" ];
-    };
-
-    dconf.settings = {
-      "ca/desrt/dconf-editor" = { show-warning = false; };
-
-      "org/gnome/desktop/peripherals/mouse" = {
-        accel-profile = "adaptive";
-        left-handed = false;
-        natural-scroll = false;
-      };
-
-      "org/gnome/desktop/peripherals/touchpad" = {
-        natural-scroll = true;
-        tap-to-click = true;
-        two-finger-scrolling-enabled = true;
-      };
-    };
+      ### File Manager
+      gnome.gvfs # Virtual Filesystem support library
+      cifs-utils # Tools for managing Linux CIFS client filesystems
+    ];
   };
 }
