cfeebb42e85f94662a6737eca8ad436a60033ffb
Reinaldo <reinaldo800@gmail.com>
2024-04-05 15:33:02 -0300

    fix

--- home-manager/_mixins/desktop/bspwm/default.nix
+++ home-manager/_mixins/desktop/bspwm/default.nix
@@ -1,54 +1,53 @@
 { pkgs, config, lib, ... }:
 let
   nixgl = import ../../../../lib/nixGL.nix { inherit config pkgs; };
-  alacrity =
-    in
+in
+{
+
+  config = {
+    home =
+      let
+        gio = pkgs.gnome.gvfs;
+      in
       {
+        packages = with pkgs; [
+          ### Window
+          bpswm
+          wmname
+          sxhkd
+          dunst
+          rofi
+          polybar
 
-        config = {
-          home =
-            let
-              gio = pkgs.gnome.gvfs;
-            in
-            {
-              packages = with pkgs; [
-                ### Window
-                bpswm
-                wmname
-                sxhkd
-                dunst
-                rofi
-                polybar
+          ### Theme
+          lxappearance-gtk2
 
-                ### Theme
-                lxappearance-gtk2
+          ### File Manager
+          gio # Virtual Filesystem support library
+          cifs-utils # Tools for managing Linux CIFS client filesystems
 
-                ### File Manager
-                gio # Virtual Filesystem support library
-                cifs-utils # Tools for managing Linux CIFS client filesystems
+          # Utils
+          pamixer # Pulseaudio command line mixer
+          imagemagick
+          lm_sensors
+        ];
 
-                # Utils
-                pamixer # Pulseaudio command line mixer
-                imagemagick
-                lm_sensors
-              ];
-
-              sessionVariables = {
-                "_JAVA_AWT_WM_NONREPARENTING" = "1";
-                GIO_EXTRA_MODULES = "${gio}/lib/gio/modules";
-              };
-
-              sessionPath = [ "$HOME/.local/bin" ];
-            };
-
-          dconf.settings = { };
-
-          programs = {
-            alacritty = {
-              enable = true;
-              package = nixgl pkgs.alacritty;
-              settings = import ../../apps/terminal/alacritty.nix { inherit config; };
-            };
-          };
+        sessionVariables = {
+          "_JAVA_AWT_WM_NONREPARENTING" = "1";
+          GIO_EXTRA_MODULES = "${gio}/lib/gio/modules";
         };
-      }
+
+        sessionPath = [ "$HOME/.local/bin" ];
+      };
+
+    dconf.settings = { };
+
+    programs = {
+      alacritty = {
+        enable = true;
+        package = nixgl pkgs.alacritty;
+        settings = import ../../apps/terminal/alacritty.nix { inherit config; };
+      };
+    };
+  };
+}
