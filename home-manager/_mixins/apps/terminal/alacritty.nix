2c5da7581f2edd484efc5bbf73847b7ef2467fed
Reinaldo <reinaldo800@gmail.com>
2024-04-06 14:44:08 -0300

    testing

--- home-manager/_mixins/apps/terminal/alacritty.nix
+++ home-manager/_mixins/apps/terminal/alacritty.nix
@@ -1,13 +1,8 @@
 { pkgs, lib ? pkgs.lib, ... }:
-# with lib.hm.gvariant;
 let
   _ = lib.getExe;
-  #   nixGL = import ../../../../lib/nixGL.nix { inherit config pkgs; };
 in
 {
-  # programs.alacritty = {
-  # enable = true;
-  # package = pkgs.alacritty;
   settings = {
     window = {
       title = "Terminal";
@@ -16,37 +11,52 @@ in
         general = "Alacritty";
       };
       padding = {
-        x = 15;
-        y = 15;
+        x = 10;
+        y = 10;
       };
       dimensions = {
-        lines = 75;
-        columns = 100;
+        lines = 35;
+        columns = 110;
       };
+      # opacity = 0.7;
+      opacity = 0.95;
+      decorations = "full";
+      startup_mode = "Windowed";
+      dynamic_title = true;
 
-      opacity = 0.7;
+      ## Background opacity
+      # background_opacity: 1.0
+    };
+
+    ## Set environment variables
+    env = {
+      TERM = "alacritty";
+      WINIT_X11_SCALE_FACTOR = "1.0";
     };
 
     font = {
       normal = {
-        # family = "JetbrainsMono Nerd Font";
+        family = "JetbrainsMono Nerd Font";
         # style = "Regular";
-        family = "FiraCode Nerd Font Mono";
-        style = "Retina";
+        # family = "FiraCode Nerd Font Mono";
+        # style = "Retina";
       };
-      # italic = {
-      #   family = "FiraCode Nerd Font Mono";
-      #   style = "MediumItalic";
-      # };
-      # bold = {
-      #   family = "FiraCode Nerd Font Mono";
-      #   style = "Bold";
-      # };
-      # bold_italic = {
-      #   family = "FiraCode Nerd Font Mono";
-      #   style = "BoldItalic";
-      # };
-      size = 11.0;
+      italic = {
+        family = "JetbrainsMono Nerd Font";
+        #   family = "FiraCode Nerd Font Mono";
+        #   style = "MediumItalic";
+      };
+      bold = {
+        family = "JetbrainsMono Nerd Font";
+        #   family = "FiraCode Nerd Font Mono";
+        #   style = "Bold";
+      };
+      bold_italic = {
+        family = "JetbrainsMono Nerd Font";
+        #   family = "FiraCode Nerd Font Mono";
+        #   style = "BoldItalic";
+      };
+      size = 12.0;
     };
 
     draw_bold_text_with_bright_colors = true;
@@ -60,33 +70,103 @@ in
         foreground = "0xEBEBEB";
       };
       cursor = {
-        text = "0xFF261E";
-        cursor = "0xFF261E";
+        # text = "0xFF261E";
+        # cursor = "0xFF261E";
+        text = "#24273A"; # base
+        cursor = "#B7BDF8"; # lavender
       };
       normal = {
-        black = "0x0D0D0D";
-        red = "0xFF301B";
-        green = "0xA0E521";
-        yellow = "0xFFC620";
-        blue = "0x178AD1";
-        magenta = "0x9f7df5";
-        cyan = "0x21DEEF";
-        white = "0xEBEBEB";
+        black = "#494D64"; # surface1
+        red = "#ED8796"; # red
+        green = "#A6DA95"; # green
+        yellow = "#EED49F"; # yellow
+        blue = "#8AADF4"; # blue
+        magenta = "#F5BDE6"; # pink
+        cyan = "#8BD5CA"; # teal
+        white = "#B8C0E0"; # subtext1
       };
       bright = {
-        black = "0x6D7070";
-        red = "0xFF4352";
-        green = "0xB8E466";
-        yellow = "0xFFD750";
-        blue = "0x1BA6FA";
-        magenta = "0xB978EA";
-        cyan = "0x73FBF1";
-        white = "0xFEFEF8";
+        black = "#5B6078"; # surface2
+        red = "#ED8796"; # red
+        green = "#A6DA95"; # green
+        yellow = "#EED49F"; # yellow
+        blue = "#8AADF4"; # blue
+        magenta = "#F5BDE6"; # pink
+        cyan = "#8BD5CA"; # teal
+        white = "#A5ADCB"; # subtext0
+      };
+
+      # Dim colors
+      dim = {
+        black = "#494d64"; #surface1
+        red = "#ED8796"; # red
+        green = "#A6DA95"; # green
+        yellow = "#EED49F"; # yellow
+        blue = "#8AADF4"; # blue
+        magenta = "#F5BDE6"; # pink
+        cyan = "#8BD5CA"; # teal
+        white = "#B8C0E0"; # subtext1
+      };
+
+      indexed_colors = [
+        {
+          index = 16;
+          color = "#F5A97F";
+        }
+        {
+          index = 17;
+          color = "#F4DBD6";
+        }
+      ];
+
+      # Search colors
+      search = {
+        matches = {
+          foreground = "#24273A"; # base
+          background = "#A5ADCB"; # subtext0
+          focused_match = {
+            foreground = "#24273A"; # base
+            background = "#A6DA95"; # green
+          };
+          footer_bar = {
+            foreground = "#24273A"; # base
+            background = "#A5ADCB"; # subtext0
+          };
+        };
+      };
+
+      # Keyboard regex hints
+      hints = {
+        start = {
+          foreground = "#24273A"; # base
+          background = "#EED49F"; # yellow
+        };
+        end = {
+          foreground = "#24273A"; # base
+          background = "#A5ADCB"; # subtext0
+        };
+      };
+
+      # Selection colors
+      selection = {
+        text = "#24273A"; # base
+        background = "#F4DBD6"; # rosewater
       };
     };
+
+    ## scrolling
     scrolling = {
       history = 10000;
+      multiplier = 3;
+    };
+
+    ## Cursor
+    cursor = {
+      style = {
+        shape = "Block";
+        blinking = "On";
+      };
+      unfocused_hollow = false;
     };
   };
-  # };
 }
--- home-manager/_mixins/desktop/bspwm/default.nix
+++ home-manager/_mixins/desktop/bspwm/default.nix
@@ -55,38 +55,63 @@ in
           alwaysResetDesktops = true;
           monitors = {
             eDP-1 = [ "I" "II" "III" "IV" "V" "VI" "VII" "VIII" "IX" ];
+            # bspc monitor eDP-1 -d 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰮯 󰮯
           };
-          extraConfig = ''
-            # if [[ $(xrandr -1 | grep 'HDMI-1 connected') ]]; then
-            # 	xrandr --output eDP-1 --primary --mode 1920x1080 --rotate normal --output HDMI-1 --mode 1600x900 --rotate normal --right-of eDP-1
-            # fi
-            bspc monitor eDP-1 -d 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰮯 󰮯
+          extraConfigEarly = ''
+            xsetroot -cursor_name left_ptr
 
-            wmname LG3D
-
-            bspc config pointer_modifier mod4
-            bspc config pointer_action1 move
-            bspc config pointer_action2 resize_side
-            bspc config pointer_action2 resize_corner
-
-            bspc config border_width         2
-            bspc config window_gap           10
-
-            bspc config split_ratio          0.52
-            bspc config borderless_monocle   true
-            bspc config gapless_monocle      true
-
-            bspc config normal_border_color     "#1E1F29"
-            bspc config focused_border_color    "#BD93F9"
-            bspc config presel_border_color     "#FF79C6"
-
-            bspc rule -a Chromium desktop='^2'
-            bspc rule -a Blueman-manager state=floating center=true
-            bspc rule -a kitty state=pseudo_tiled center=true
-            bspc rule -a mplayer2 state=floating
-            bspc rule -a Kupfer.py focus=on
-            bspc rule -a Screenkey manage=off
+            # picom
+            pkill picom
+            picom -b &
+            #picom --expiremental-backends --no-use-damage &
           '';
+          settings = {
+            pointer_modifier = "mod4";
+            pointer_action1 = "move";
+            # pointer_action2 = "resize_side";
+            pointer_action2 = "resize_corner";
+
+            border_width = 2;
+            window_gap = 10;
+
+            split_ratio = 0.52;
+            borderless_monocle = true;
+            gapless_monocle = true;
+
+            normal_border_color = "#1E1F29";
+            focused_border_color = "#BD93F9";
+            presel_border_color = "#FF79C6";
+          };
+
+          rules = {
+            "Chromium" = {
+              desktop = "^2";
+            };
+            "Blueman-manager" = {
+              state = "floating";
+              center = true;
+            };
+            "Alacritty" = {
+              state = "pseudo_tiled";
+              center = true;
+            };
+            "Mpv" = {
+              # "mplayer2"
+              state = "floating";
+            };
+            "Kupfer.py" = {
+              focus = "on";
+            };
+            "Screenkey" = {
+              manage = "off";
+            };
+          };
+          # bspc rule -a Chromium desktop='^2'
+          # bspc rule -a Blueman-manager state=floating center=true
+          # bspc rule -a kitty state=pseudo_tiled center=true
+          # bspc rule -a mplayer2 state=floating
+          # bspc rule -a Kupfer.py focus=on
+          # bspc rule -a Screenkey manage=off
         };
       };
     };
