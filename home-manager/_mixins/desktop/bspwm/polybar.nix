a14601145f7d11c2e9da2c45a92fbfa7cce391e4
Reinaldo <reinaldo800@gmail.com>
2024-04-08 14:24:19 -0300

    fix

--- home-manager/_mixins/desktop/bspwm/polybar.nix
+++ home-manager/_mixins/desktop/bspwm/polybar.nix
@@ -87,7 +87,7 @@ in
     polybar = {
       enable = true;
       package = nixgl pkgs.unstable.polybarFull;
-      script = "sleep 0.5; polybar bar/everforest &";
+      script = "sleep 1; polybar -q bar/everforest &";
       settings = {
         ##################
         ### Config.ini ###
