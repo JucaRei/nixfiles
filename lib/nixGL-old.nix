2c9250cecfa987ab66b9c74bada9ef05bf9d6fd8
Reinaldo <reinaldo800@gmail.com>
2024-04-15 17:22:47 -0300

    z

--- lib/nixGL-old.nix
+++ lib/nixGL-old.nix
@@ -4,14 +4,14 @@
 ,
 }:
 let
-  pkgs.nixGL = (import
+  nvidiaGL = (import
     (builtins.fetchGit {
       url = "http://github.com/guibou/nixGL";
       ref = "refs/heads/backport/noGLVND";
     })
     # { enable32bits = false; }).auto;
     # ).auto;
-  ).nixGLNvidia;
+  ).auto;
 in
 # Wrap a single package
 pkg:
@@ -39,7 +39,7 @@ pkg:
     shopt -s nullglob # Prevent loop from running if no files
     for file in ${pkg.out}/bin/*; do
       echo "#!${pkgs.bash}/bin/bash" > "$out/bin/$(basename $file)"
-      echo "exec -a \"\$0\" ${pkgs.nixGL.nixGLNvidia}/bin/nixGLNvidia $file \"\$@\"" >> "$out/bin/$(basename $file)"
+      echo "exec -a \"\$0\" ${pkgs.nvidiaGL.auto}/bin/nixGLNvidia $file \"\$@\"" >> "$out/bin/$(basename $file)"
       chmod +x "$out/bin/$(basename $file)"
     done
     shopt -u nullglob # Revert nullglob back to its normal default state
