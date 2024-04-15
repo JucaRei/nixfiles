1bdad30064dd8203c82421d182a818b5c2c7327a
Reinaldo <reinaldo800@gmail.com>
2024-04-15 17:39:06 -0300

    fix

--- home-manager/hosts/oldarch.nix
+++ home-manager/hosts/oldarch.nix
@@ -6,7 +6,7 @@ let
   #     ref = "refs/heads/backport/noGLVND";
   #   })
   #   { enable32bits = true; }).auto;
-  nixGL-old = import ../../lib/nixGL-old.nix { inherit config pkgs lib; };
+  nixGL-old = import ../../lib/nixGL-old.nix { inherit config pkgs; };
 in
 {
   imports = [
