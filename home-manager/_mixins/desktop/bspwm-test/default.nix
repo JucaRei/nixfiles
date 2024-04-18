746ba28d01c4ec113484fe047e718bd962262061
Reinaldo <reinaldo800@gmail.com>
2024-04-18 12:18:16 -0300

    testing

--- home-manager/_mixins/desktop/bspwm-test/default.nix
+++ home-manager/_mixins/desktop/bspwm-test/default.nix
@@ -1,6 +1,8 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
 let
   nixgl = import ../../../../lib/nixGL.nix { inherit config pkgs; };
+  _ = lib.getExe;
+  windowMan = "${_ config.xsession.windowManager.bspwm.package}/bin/bspwm";
 in
 {
   config = {
@@ -21,26 +23,61 @@ in
         ".xinitrc" = {
           executable = true;
           text = ''
-              [ -f ~/.xprofile ] && . ~/.xprofile
-              [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
+            #!/bin/sh
 
-              if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-                  eval $(dbus-launch --exit-with-session --sh-syntax)
-              fi
+            userresources=$HOME/.Xresources
+            usermodmap=$HOME/.Xmodmap
+            sysresources=/etc/X11/xinit/.Xresources
+            sysmodmap=/etc/X11/xinit/.Xmodmap
 
-              ${pkgs.systemd}/bin/systemctl --user import-environment DISPLAY XAUTHORITY
+            # merge in defaults and keymaps
 
-              if command -v dbus-update-activation-environment >/dev/null 2>&1; then
-            	  dbus-update-activation-environment DISPLAY XAUTHORITY
-              fi
+            if [ -f $sysresources ]; then
 
-              ${pkgs.systemd}/bin/systemctl --user start graphical-session.target
 
-              ${pkgs.autorandr}/bin/autorandr --change
 
-              ${pkgs.sxhkd}/bin/sxhkd &
 
-              exec ${(nixgl pkgs.bspwm)}/bin/bspwm
+
+
+
+                xrdb -merge $sysresources
+
+            fi
+
+            if [ -f $sysmodmap ]; then
+                xmodmap $sysmodmap
+            fi
+
+            if [ -f "$userresources" ]; then
+
+
+
+
+
+
+
+                xrdb -merge "$userresources"
+
+            fi
+
+            if [ -f "$usermodmap" ]; then
+                xmodmap "$usermodmap"
+            fi
+
+            # start some nice programs
+
+            if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+             for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+              [ -x "$f" ] && . "$f"
+             done
+             unset f
+            fi
+
+            # twm &
+            # xclock -geometry 50x50-1+1 &
+            # xterm -geometry 80x50+494+51 &
+            # xterm -geometry 80x20+494-0 &
+            exec ${windowMan}
           '';
         };
       };
