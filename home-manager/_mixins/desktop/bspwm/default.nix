216f4d5e9a7c09eec4b0bbf81bbeca2d6dacaf2e
Reinaldo <reinaldo800@gmail.com>
2024-04-18 16:30:54 -0300

    test

--- home-manager/_mixins/desktop/bspwm/default.nix
+++ home-manager/_mixins/desktop/bspwm/default.nix
@@ -129,30 +129,55 @@ in
           #   gesture swipe left 3 bspc desktop -f prev.local
           # '';
 
-          # ".xinitrc" = {
-          #   executable = true;
-          #   text = ''
-          #       [ -f ~/.xprofile ] && . ~/.xprofile
-          #       [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
+          ".xinitrc" = {
+            executable = true;
+            text = ''
+              #!/bin/sh
 
-          #       if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-          #           eval $(dbus-launch --exit-with-session --sh-syntax)
-          #       fi
+              userresources=$HOME/.Xresources
+              usermodmap=$HOME/.Xmodmap
+              sysresources=/etc/X11/xinit/.Xresources
+              sysmodmap=/etc/X11/xinit/.Xmodmap
 
-          #       ${pkgs.systemd}/bin/systemctl --user import-environment DISPLAY XAUTHORITY
+              # merge in defaults and keymaps
 
-          #       if command -v dbus-update-activation-environment >/dev/null 2>&1; then
-          #     	  dbus-update-activation-environment DISPLAY XAUTHORITY
-          #       fi
+              if [ -f $sysresources ]; then
+                  ${pkgs.xorg.xrdb}/bin/xrdb -merge $sysresources
+              fi
 
-          #       ${pkgs.systemd}/bin/systemctl --user start graphical-session.target
+              if [ -f $sysmodmap ]; then
+                  ${pkgs.xorg.xmodmap}/bin/xmodmap $sysmodmap
+              fi
 
-          #       ${pkgs.autorandr}/bin/autorandr --change
+              if [ -f "$userresources" ]; then
+                  ${pkgs.xorg.xrdb}/bin/xrdb -merge "$userresources"
+              fi
 
-          #       ${pkgs.sxhkd}/bin/sxhkd &
+              if [ -f "$usermodmap" ]; then
+                  ${pkgs.xorg.xmodmap}/bin/xmodmap "$usermodmap"
+              fi
 
-          #       exec ${(nixgl pkgs.bspwm)}/bin/bspwm
-          #   '';
+              if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+                eval $(dbus-launch --exit-with-session --sh-syntax)
+              fi
+              systemctl --user import-environment DISPLAY XAUTHORITY
+
+              if command -v dbus-update-activation-environment > /dev/null 2>&1; then
+                dbus-update-activation-environment DISPLAY XAUTHORITY
+              fi
+
+              # start some nice programs
+
+              if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+               for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+                [ -x "$f" ] && . "$f"
+               done
+               unset f
+              fi
+
+              exec ${windowMan} &
+            '';
+          };
         };
       };
 
