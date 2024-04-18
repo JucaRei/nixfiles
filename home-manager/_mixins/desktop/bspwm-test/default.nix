5191cf5bebcb67284ff79f0178fc0d633ebc2a8e
Reinaldo <reinaldo800@gmail.com>
2024-04-18 14:03:31 -0300

    test

--- home-manager/_mixins/desktop/bspwm-test/default.nix
+++ home-manager/_mixins/desktop/bspwm-test/default.nix
@@ -20,60 +20,61 @@ in
         xorg.xrandr
         xfce.xfce4-terminal
       ];
-      file = {
-        ".xinitrc" = {
-          executable = true;
-          text = ''
-            #!/bin/sh
+      # file = {
+      #   ".xinitrc" = {
+      #     executable = true;
+      #     text = ''
+      #       #!/bin/sh
 
-            userresources=$HOME/.Xresources
-            usermodmap=$HOME/.Xmodmap
-            sysresources=/etc/X11/xinit/.Xresources
-            sysmodmap=/etc/X11/xinit/.Xmodmap
+      #       userresources=$HOME/.Xresources
+      #       usermodmap=$HOME/.Xmodmap
+      #       sysresources=/etc/X11/xinit/.Xresources
+      #       sysmodmap=/etc/X11/xinit/.Xmodmap
 
-            # merge in defaults and keymaps
+      #       # merge in defaults and keymaps
 
-            if [ -f $sysresources ]; then
-                ${pkgs.xorg.xrdb}/bin/xrdb -merge $sysresources
-            fi
+      #       if [ -f $sysresources ]; then
+      #           ${pkgs.xorg.xrdb}/bin/xrdb -merge $sysresources
+      #       fi
 
-            if [ -f $sysmodmap ]; then
-                ${pkgs.xorg.xmodmap}/bin/xmodmap $sysmodmap
-            fi
+      #       if [ -f $sysmodmap ]; then
+      #           ${pkgs.xorg.xmodmap}/bin/xmodmap $sysmodmap
+      #       fi
 
-            if [ -f "$userresources" ]; then
-                ${pkgs.xorg.xrdb}/bin/xrdb -merge "$userresources"
-            fi
+      #       if [ -f "$userresources" ]; then
+      #           ${pkgs.xorg.xrdb}/bin/xrdb -merge "$userresources"
+      #       fi
 
-            if [ -f "$usermodmap" ]; then
-                ${pkgs.xorg.xmodmap}/bin/xmodmap "$usermodmap"
-            fi
+      #       if [ -f "$usermodmap" ]; then
+      #           ${pkgs.xorg.xmodmap}/bin/xmodmap "$usermodmap"
+      #       fi
 
-            if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-              eval $(dbus-launch --exit-with-session --sh-syntax)
-            fi
-            systemctl --user import-environment DISPLAY XAUTHORITY
+      #       if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+      #         eval $(dbus-launch --exit-with-session --sh-syntax)
+      #       fi
+      #       systemctl --user import-environment DISPLAY XAUTHORITY
 
-            if command -v dbus-update-activation-environment > /dev/null 2>&1; then
-              dbus-update-activation-environment DISPLAY XAUTHORITY
-            fi
+      #       if command -v dbus-update-activation-environment > /dev/null 2>&1; then
+      #         dbus-update-activation-environment DISPLAY XAUTHORITY
+      #       fi
 
-            # start some nice programs
+      #       # start some nice programs
 
-            if [ -d /etc/X11/xinit/xinitrc.d ] ; then
-             for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
-              [ -x "$f" ] && . "$f"
-             done
-             unset f
-            fi
+      #       if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+      #        for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+      #         [ -x "$f" ] && . "$f"
+      #        done
+      #        unset f
+      #       fi
 
-            exec ${windowMan} &
-          '';
-        };
-      };
+      #       exec ${windowMan} &
+      #     '';
+      #   };
+      # };
     };
     xsession = {
       enable = true;
+      initExtra = "exec ${windowMan} &";
       windowManager = {
         bspwm = {
           enable = true;
