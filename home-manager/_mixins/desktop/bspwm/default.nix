3dbb92425ed4fc56e576027f0f743b3c104b1435
Reinaldo <reinaldo800@gmail.com>
2024-04-18 14:14:28 -0300

    testing

--- home-manager/_mixins/desktop/bspwm/default.nix
+++ home-manager/_mixins/desktop/bspwm/default.nix
@@ -117,289 +117,290 @@ in
           #   gesture swipe left 3 bspc desktop -f prev.local
           # '';
 
-          ".xinitrc" = {
-            executable = true;
-            text = ''
-                [ -f ~/.xprofile ] && . ~/.xprofile
-                [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
+          # ".xinitrc" = {
+          #   executable = true;
+          #   text = ''
+          #       [ -f ~/.xprofile ] && . ~/.xprofile
+          #       [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
 
-                if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
-                    eval $(dbus-launch --exit-with-session --sh-syntax)
-                fi
+          #       if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+          #           eval $(dbus-launch --exit-with-session --sh-syntax)
+          #       fi
 
-                ${pkgs.systemd}/bin/systemctl --user import-environment DISPLAY XAUTHORITY
+          #       ${pkgs.systemd}/bin/systemctl --user import-environment DISPLAY XAUTHORITY
 
-                if command -v dbus-update-activation-environment >/dev/null 2>&1; then
-              	  dbus-update-activation-environment DISPLAY XAUTHORITY
-                fi
+          #       if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+          #     	  dbus-update-activation-environment DISPLAY XAUTHORITY
+          #       fi
 
-                ${pkgs.systemd}/bin/systemctl --user start graphical-session.target
+          #       ${pkgs.systemd}/bin/systemctl --user start graphical-session.target
 
-                ${pkgs.autorandr}/bin/autorandr --change
+          #       ${pkgs.autorandr}/bin/autorandr --change
 
-                ${pkgs.sxhkd}/bin/sxhkd &
+          #       ${pkgs.sxhkd}/bin/sxhkd &
 
-                exec ${(nixgl pkgs.bspwm)}/bin/bspwm
-            '';
-          };
+          #       exec ${(nixgl pkgs.bspwm)}/bin/bspwm
+          #   '';
         };
       };
+  };
 
-    dconf.settings = { };
-    xsession = {
-      enable = true;
-      initExtra = "exec ${windowMan} &";
-      windowManager = {
-        bspwm = {
-          enable = true;
-          # package = (nixgl pkgs.unstable.bspwm);
-          package = nixgl pkgs.bspwm;
-          startupPrograms = [
-            "pgrep -x sxhkd > /dev/null || sxhkd"
-            # "nitrogen --restore"
-            "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-            "sleep 2; polybar -q everforest"
-            # "thunar --daemon"
-            # "${pkgs.flameshot}/bin/flameshot"
-            # "${pkgs.feh}/bin/feh --bg-scale ${config.my.settings.wallpaper}"
-          ];
-          alwaysResetDesktops = true;
-          monitors = {
-            Virtual-1 = [ "I" "II" "III" "IV" "V" "VI" "VII" "VIII" ];
-            HDMI-1-0 = [ "I" "II" "III" "IV" "V" "VI" "VII" "VIII" ];
-            eDP-1 = [ "I" "II" "III" "IV" "V" "VI" "VII" "VIII" ];
-            # bspc monitor eDP-1 -d 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰮯 󰮯
-          };
-          extraConfigEarly = ''
-            wmname LG3D
-            # picom
-            # pkill picom
-            picom -b --legacy-backends --no-use-damage &
-            #picom --expiremental-backends --no-use-damage &
-
-            ### Only have workspaces for primary monitor
-            export MONITOR=$(xrandr -q | grep primary | cut -d' ' -f1)
-            export MONITORS=( $(xrandr -q | grep ' connected' | cut -d' ' -f1) )
-            MONITOR=$\{MONITOR:-$\{MONITORS[0]}}
-
-            bspc config remove_disabled_monitors true
-            bspc config remove_unplugged_monitors true
-          '';
-          extraConfig = ''
-            ${pkgs.systemd}/bin/systemctl --user start bspwm-session.target
-
-            # ${pkgs.autorandr}/bin/autorandr --change
-
-            ${pkgs.xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr
-            # Wait for the network to be up
-            # ${pkgs.libnotify}/bin/notify-send 'Waiting for network...'
-            # while ! systemctl is-active --quiet network-online.target; do sleep 1; done
-            # notify-send 'Network found.'
-
-            # Set background and top bar
-            # ${pkgs.feh}/bin/feh --bg-scale $HOME/.local/share/img/wallpaper/active
-          '';
-          settings = {
-            remove_disabled_monitors = true;
-            remove_unplugged_monitors = true;
-            pointer_modifier = "mod4";
-            pointer_action1 = "move";
-            pointer_action2 = "resize_side";
-            pointer_action3 = "resize_corner";
-            click_to_focus = "button1";
-            focus_follows_pointer = true;
-            border_width = 2;
-            window_gap = 10;
-            automatic_scheme = "tiling";
-            initial_polarity = "first_child";
-            split_ratio = 0.52;
-            single_monocle = true;
-            borderless_monocle = true;
-            gapless_monocle = false;
-            paddingless_mono = true;
-            normal_border_color = "#343c40"; # "#1E1F29";
-            active_border_color = "#DBBC7F";
-            focused_border_color = "#DBBC7F"; # "#BD93F9";
-            presel_border_color = "#343c40"; #"#FF79C6";
-          };
-
-          rules = {
-            "chromium" = {
-              desktop = "^3";
-              focus = true;
-            };
-            "firefox" = {
-              desktop = "^3";
-              focus = true;
-            };
-            "Blueman-manager" = {
-              state = "floating";
-              center = true;
-            };
-            "Alacritty" = {
-              state = "pseudo_tiled";
-              center = true;
-            };
-            "Alacritty:floating" = {
-              state = "floating";
-            };
-            "join*" = {
-              state = "floating";
-            };
-            "st:floating" = {
-              state = "floating";
-            };
-            "mpv" = {
-              # "mplayer2"
-              state = "floating";
-              # rectangle = "1200x700+360+190";
-              # desktop = "^6";
-              sticky = true;
-            };
-            # "Kupfer.py" = {
-            #   focus = "on";
-            # };
-            # "Screenkey" = {
-            #   manage = "off";
-            # };
-            "Pavucontrol" = {
-              state = "floating";
-            };
-          };
-        };
-      };
-    };
-
-    gtk = {
-      enable = true;
-      iconTheme = {
-        name = "Papirus-Dark";
-        package = pkgs.papirus-icon-theme;
-      };
-      theme = {
-        package = pkgs.solarc-gtk-theme;
-        name = "SolArc";
-        # name = "zukitre-dark";
-        # package = pkgs.zuki-themes;
-      };
-      # gtk3.extraConfig = {
-      #   Settings = ''
-      #     gtk-application-prefer-dark-theme=1
-      #   '';
-      # };
-      # gtk4.extraConfig = {
-      #   Settings = ''
-      #     gtk-application-prefer-dark-theme=1
-      #   '';
-      # };
-    };
-
-    services = {
-      sxhkd = {
+  dconf.settings = { };
+  xsession = {
+    enable = true;
+    # initExtra = "exec ${windowMan} &";
+    command = "exec ${windowMan} &";
+    windowManager = {
+      bspwm = {
         enable = true;
-        keybindings = import ./sxhkdrc.nix args;
-      };
-      fusuma = {
-        enable = true;
-        extraPackages = with pkgs;[ xdo xdotool coreutils xorg.xprop ];
-        settings = {
-          swipe = {
-            "3" = {
-              left = {
-                # GNOME: Switch to left workspace
-                # command = "xdotool key shift+l"; # "xdotool key ctrl+alt+Right";
-                command = "bspc desktop -f {prev}.local";
-                # left:
-                #     command: exec i3 focus left
-                # right:
-                #     command: exec i3 focus right
-                # up:
-                #     command: exec i3 focus down
-                # down:
-              };
-              right = {
-                # command = "xdotool key shift+h";
-                command = "bspc desktop -f {next}.local";
-              };
-            };
-            # "4" = {
-            #   left = {
-            #     command = "i3-msg 'workspace prev";
-            #   };
-            #   right = {
-            #     command = "i3-msg 'workspace next";
-            #   };
-          };
-        };
-      };
-    };
-
-    programs = {
-      alacritty = {
-        enable = true;
-        package = nixgl pkgs.alacritty;
-        settings = import ../../apps/terminal/alacritty.nix args;
-      };
-      rofi = import ./rofi.nix args;
-      feh = {
-        enable = true;
-        # package = pkgs.feh;
-        # keybindings = "";
-        # buttons = "";
-      };
-    };
-
-    i18n = {
-      # glibcLocales = pkgs.glibcLocales.override {
-      #   allLocales = false;
-      #   locales = [ "en_US.UTF-8/UTF-8" "pt_BR.UTF-8/UTF-8" ];
-      # };
-      inputMethod = {
-        enabled = "fcitx5";
-        fcitx5.addons = with pkgs; [
-          fcitx5-rime
+        # package = (nixgl pkgs.unstable.bspwm);
+        package = nixgl pkgs.bspwm;
+        startupPrograms = [
+          "pgrep -x sxhkd > /dev/null || sxhkd"
+          # "nitrogen --restore"
+          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
+          "sleep 2; polybar -q everforest"
+          # "thunar --daemon"
+          # "${pkgs.flameshot}/bin/flameshot"
+          # "${pkgs.feh}/bin/feh --bg-scale ${config.my.settings.wallpaper}"
         ];
-      };
-    };
+        alwaysResetDesktops = true;
+        monitors = {
+          Virtual-1 = [ "I" "II" "III" "IV" "V" "VI" "VII" "VIII" ];
+          HDMI-1-0 = [ "I" "II" "III" "IV" "V" "VI" "VII" "VIII" ];
+          eDP-1 = [ "I" "II" "III" "IV" "V" "VI" "VII" "VIII" ];
+          # bspc monitor eDP-1 -d 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰊠 󰮯 󰮯
+        };
+        extraConfigEarly = ''
+          wmname LG3D
+          # picom
+          # pkill picom
+          picom -b --legacy-backends --no-use-damage &
+          #picom --expiremental-backends --no-use-damage &
 
-    systemd.user = {
-      targets.bspwm-session = {
-        Unit = {
-          Description = "Bspwm session";
-          BindsTo = [ "graphical-session.target" ];
-          Wants = [ "graphical-session-pre.target" ];
-          After = [ "graphical-session-pre.target" ];
+          ### Only have workspaces for primary monitor
+          export MONITOR=$(xrandr -q | grep primary | cut -d' ' -f1)
+          export MONITORS=( $(xrandr -q | grep ' connected' | cut -d' ' -f1) )
+          MONITOR=$\{MONITOR:-$\{MONITORS[0]}}
+
+          bspc config remove_disabled_monitors true
+          bspc config remove_unplugged_monitors true
+        '';
+        extraConfig = ''
+          ${pkgs.systemd}/bin/systemctl --user start bspwm-session.target
+
+          # ${pkgs.autorandr}/bin/autorandr --change
+
+          ${pkgs.xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr
+          # Wait for the network to be up
+          # ${pkgs.libnotify}/bin/notify-send 'Waiting for network...'
+          # while ! systemctl is-active --quiet network-online.target; do sleep 1; done
+          # notify-send 'Network found.'
+
+          # Set background and top bar
+          # ${pkgs.feh}/bin/feh --bg-scale $HOME/.local/share/img/wallpaper/active
+        '';
+        settings = {
+          remove_disabled_monitors = true;
+          remove_unplugged_monitors = true;
+          pointer_modifier = "mod4";
+          pointer_action1 = "move";
+          pointer_action2 = "resize_side";
+          pointer_action3 = "resize_corner";
+          click_to_focus = "button1";
+          focus_follows_pointer = true;
+          border_width = 2;
+          window_gap = 10;
+          automatic_scheme = "tiling";
+          initial_polarity = "first_child";
+          split_ratio = 0.52;
+          single_monocle = true;
+          borderless_monocle = true;
+          gapless_monocle = false;
+          paddingless_mono = true;
+          normal_border_color = "#343c40"; # "#1E1F29";
+          active_border_color = "#DBBC7F";
+          focused_border_color = "#DBBC7F"; # "#BD93F9";
+          presel_border_color = "#343c40"; #"#FF79C6";
+        };
+
+        rules = {
+          "chromium" = {
+            desktop = "^3";
+            focus = true;
+          };
+          "firefox" = {
+            desktop = "^3";
+            focus = true;
+          };
+          "Blueman-manager" = {
+            state = "floating";
+            center = true;
+          };
+          "Alacritty" = {
+            state = "pseudo_tiled";
+            center = true;
+          };
+          "Alacritty:floating" = {
+            state = "floating";
+          };
+          "join*" = {
+            state = "floating";
+          };
+          "st:floating" = {
+            state = "floating";
+          };
+          "mpv" = {
+            # "mplayer2"
+            state = "floating";
+            # rectangle = "1200x700+360+190";
+            # desktop = "^6";
+            sticky = true;
+          };
+          # "Kupfer.py" = {
+          #   focus = "on";
+          # };
+          # "Screenkey" = {
+          #   manage = "off";
+          # };
+          "Pavucontrol" = {
+            state = "floating";
+          };
         };
       };
-
-      services.polkit-gnome-authentication-agent-1 = {
-        Unit.Description = "polkit-gnome-authentication-agent-1";
-        Install.WantedBy = [ "graphical-session.target" ];
-        Service = {
-          Type = "simple";
-          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-      };
     };
+  };
 
-    # systemd.user.services.polkit-agent = {
-    #   Unit = {
-    #     Description = "launch authentication-agent-1";
-    #     After = [ "graphical-session.target" ];
-    #     PartOf = [ "graphical-session.target" ];
-    #   };
-    #   Service = {
-    #     Type = "simple";
-    #     Restart = "on-failure";
-    #     ExecStart = ''
-    #       ${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1
-    #     '';
-    #   };
-
-    #   Install = { WantedBy = [ "graphical-session.target" ]; };
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    theme = {
+      package = pkgs.solarc-gtk-theme;
+      name = "SolArc";
+      # name = "zukitre-dark";
+      # package = pkgs.zuki-themes;
+    };
+    # gtk3.extraConfig = {
+    #   Settings = ''
+    #     gtk-application-prefer-dark-theme=1
+    #   '';
+    # };
+    # gtk4.extraConfig = {
+    #   Settings = ''
+    #     gtk-application-prefer-dark-theme=1
+    #   '';
     # };
   };
+
+  services = {
+    sxhkd = {
+      enable = true;
+      keybindings = import ./sxhkdrc.nix args;
+    };
+    fusuma = {
+      enable = true;
+      extraPackages = with pkgs;[ xdo xdotool coreutils xorg.xprop ];
+      settings = {
+        swipe = {
+          "3" = {
+            left = {
+              # GNOME: Switch to left workspace
+              # command = "xdotool key shift+l"; # "xdotool key ctrl+alt+Right";
+              command = "bspc desktop -f {prev}.local";
+              # left:
+              #     command: exec i3 focus left
+              # right:
+              #     command: exec i3 focus right
+              # up:
+              #     command: exec i3 focus down
+              # down:
+            };
+            right = {
+              # command = "xdotool key shift+h";
+              command = "bspc desktop -f {next}.local";
+            };
+          };
+          # "4" = {
+          #   left = {
+          #     command = "i3-msg 'workspace prev";
+          #   };
+          #   right = {
+          #     command = "i3-msg 'workspace next";
+          #   };
+        };
+      };
+    };
+  };
+
+  programs = {
+    alacritty = {
+      enable = true;
+      package = nixgl pkgs.alacritty;
+      settings = import ../../apps/terminal/alacritty.nix args;
+    };
+    rofi = import ./rofi.nix args;
+    feh = {
+      enable = true;
+      # package = pkgs.feh;
+      # keybindings = "";
+      # buttons = "";
+    };
+  };
+
+  i18n = {
+    # glibcLocales = pkgs.glibcLocales.override {
+    #   allLocales = false;
+    #   locales = [ "en_US.UTF-8/UTF-8" "pt_BR.UTF-8/UTF-8" ];
+    # };
+    inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        fcitx5-rime
+      ];
+    };
+  };
+
+  systemd.user = {
+    targets.bspwm-session = {
+      Unit = {
+        Description = "Bspwm session";
+        BindsTo = [ "graphical-session.target" ];
+        Wants = [ "graphical-session-pre.target" ];
+        After = [ "graphical-session-pre.target" ];
+      };
+    };
+
+    services.polkit-gnome-authentication-agent-1 = {
+      Unit.Description = "polkit-gnome-authentication-agent-1";
+      Install.WantedBy = [ "graphical-session.target" ];
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
+  # systemd.user.services.polkit-agent = {
+  #   Unit = {
+  #     Description = "launch authentication-agent-1";
+  #     After = [ "graphical-session.target" ];
+  #     PartOf = [ "graphical-session.target" ];
+  #   };
+  #   Service = {
+  #     Type = "simple";
+  #     Restart = "on-failure";
+  #     ExecStart = ''
+  #       ${pkgs.mate.mate-polkit}/libexec/polkit-mate-authentication-agent-1
+  #     '';
+  #   };
+
+  #   Install = { WantedBy = [ "graphical-session.target" ]; };
+  # };
+};
 }
