821e9e4283da5aacdf752ab0715b1c85a4c6e6c3
Reinaldo <reinaldo800@gmail.com>
2024-04-22 13:09:44 -0300

    fix

--- home-manager/_mixins/config/scripts/home-man-build.nix
+++ home-manager/_mixins/config/scripts/home-man-build.nix
@@ -7,7 +7,7 @@ pkgs.writeScriptBin "home-man-build" ''
     all_cores=$(nproc)
     build_cores=$(printf "%.0f" $(echo "$all_cores * 0.75" | ${pkgs.bc}/bin/bc))
     echo "Building Nix Home-manager with $build_cores cores"
-    ${pkgs.unstable.nh}/bin/nh home switch --ask ~/.dotfiles/nixfiles/ -- --cores $build_cores
+    ${pkgs.unstable.nh}/bin/nh home switch --impure --ask ~/.dotfiles/nixfiles/ -- --cores $build_cores
   else
     ${pkgs.coreutils-full}/bin/echo "ERROR! No nix-config found in $HOME/.dotfiles/nixfiles"
   fi
