21a9b60ba324d925bfdded5777773f09f42cf237
Reinaldo <reinaldo800@gmail.com>
2024-04-24 13:17:36 -0300

    x

--- home-manager/_mixins/desktop/bspwm/everforest/polybar-everforest.nix
+++ home-manager/_mixins/desktop/bspwm/everforest/polybar-everforest.nix
@@ -96,10 +96,10 @@ in
   services = {
     polybar = {
       enable = true;
-      package = nixgl pkgs.polybar.override {
+      package = (nixgl (pkgs.polybar.override {
         pulseSupport = true;
         nlSupport = true;
-      };
+      }));
       script = "";
       settings = {
         ##################
