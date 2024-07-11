1f1f142da100acf9c65ce189ed204f776e2b581d
Reinaldo <reinaldo800@gmail.com>
2024-07-11 19:19:49 -0300

    test

--- home-manager/_mixins/services/podman.nix
+++ home-manager/_mixins/services/podman.nix
@@ -109,6 +109,12 @@ in
         };
       };
     };
+    # https://rootlesscontaine.rs/getting-started/common/cgroup2/#enabling-cpu-cpuset-and-io-delegation
+    # For minikube
+    # Writes to /etc/systemd/system/user@.service.d/overrides.conf
+    systemd.services."user@".serviceConfig = {
+      Delegate = "cpu cpuset io memory pids";
+    };
   };
 }
 
