cfb180e9eefb519a8725e40164bb48673c8b1eb1
Reinaldo <reinaldo800@gmail.com>
2024-07-12 15:19:54 -0300

    fix

--- home-manager/hosts/soyo.nix
+++ home-manager/hosts/soyo.nix
@@ -2,14 +2,14 @@
 {
   imports = [
     # ../_mixins/apps/text-editor/vscode/vscode.nix
-    "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+    # "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
   ];
   config = {
     services = {
       vscode-server = {
         # enable = lib.mkForce true;
         enableFHS = lib.mkForce true;
-        nodejsPackage = pkgs.nodejs-16_x;
+        nodejsPackage = pkgs.nodejs-18_x;
         extraRuntimeDependencies = pkgs: with pkgs; [
           nixpkgs-fmt # formatter
           nix-output-monitor # better output from builds
