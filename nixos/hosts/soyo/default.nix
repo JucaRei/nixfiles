d8a2c301df4181b21b19befed932c52298bb9ce7
Reinaldo <reinaldo800@gmail.com>
2024-07-17 16:33:29 -0300

    teste

--- nixos/hosts/soyo/default.nix
+++ nixos/hosts/soyo/default.nix
@@ -21,9 +21,9 @@
           "ahci" # Enables the Advanced Host Controller Interface (AHCI) driver, typically used for SATA (Serial ATA) controllers.
           "xhci_pci" # Enables the eXtensible Host Controller Interface (xHCI) driver for PCI-based USB controllers, providing support for USB 3.0 and later standards.
           "usbhid" # module is very simple, it takes no parameters, detects everything automatically and when a HID device is inserted, it detects it appropriately.
-          "usb_storage"
-          "sd_mod"
-          "uas"
+          "usb_storage" # Enables the USB Mass Storage driver, allowing the system to recognize and use USB storage devices like USB flash drives and external hard drives.
+          "sd_mod" # Enables the SCSI disk module (sd_mod), which allows the system to recognize and interact with SCSI-based storage devices.
+          "uas" # Enables the USB Attached SCSI (UAS) driver, which provides a faster and more efficient way to access USB storage devices.
           "sdhci_pci"
         ];
         kernelModules = [ ];
@@ -55,6 +55,7 @@
         "i915.fastboot=1"
         "enable_gvt=1"
         "mem_sleep_default=deep"
+        "quiet"
 
         "elevator=kyber" # Change IO scheduler to Kyber
         "fbcon=nodefer" # Prevent the kernel from blanking plymouth out of the framebuffer
--- nixos/users/default.nix
+++ nixos/users/default.nix
@@ -9,30 +9,41 @@ in
 
   environment.localBinInPath = true;
 
-  users.users.${username} = {
-    extraGroups = [
-      "audio"
-      "input"
-      "networkmanager"
-      "users"
-      "video"
-      "wheel"
-    ]
-    # "adbusers" "dialout" "render" "plugdev" "i2c" "systemd-journal" "corectrl" "wireshark" "storage" "scanner" "libvirtd" "qemu-libvirtd" "kvm" "input" "docker" "podman"
-    ++ ifExists [
-      "docker"
-      "lxd"
-      "podman"
-      "rtkit"
-      # "qemu-libvirtd"
-      "kvm"
-    ];
-    homeMode = "0755";
-    isNormalUser = true;
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAywaYwPN4LVbPqkc+kUc7ZVazPBDy4LCAud5iGJdr7g9CwLYoudNjXt/98Oam5lK7ai6QPItK6ECj5+33x/iFpWb3Urr9SqMc/tH5dU1b9N/9yWRhE2WnfcvuI0ms6AXma8QGp1pj/DoLryPVQgXvQlglHaDIL1qdRWFqXUO2u30X5tWtDdOoR02UyAtYBttou4K0rG7LF9rRaoLYP9iCBLxkMJbCIznPD/pIYa6Fl8V8/OVsxYiFy7l5U0RZ7gkzJv8iNz+GG8vw2NX4oIJfAR4oIk3INUvYrKvI2NSMSw5sry+z818fD1hK+soYLQ4VZ4hHRHcf4WV4EeVa5ARxdw== Martin Wimpress"
-    ];
-    packages = [ pkgs.home-manager ];
-    shell = pkgs.fish;
+  users = {
+    mutableUsers = true;
+    groups.${username} = {
+      members = [ ];
+    };
+    extraUsers.${username} = {
+      name = "${username}";
+      group = "${username}";
+    };
+    users.${username} = {
+      description = "${username}";
+      extraGroups = [
+        "audio"
+        "input"
+        "networkmanager"
+        "users"
+        "video"
+        "wheel"
+      ]
+      # "adbusers" "dialout" "render" "plugdev" "i2c" "systemd-journal" "corectrl" "wireshark" "storage" "scanner" "libvirtd" "qemu-libvirtd" "kvm" "input" "docker" "podman"
+      ++ ifExists [
+        "docker"
+        "lxd"
+        "podman"
+        "rtkit"
+        # "qemu-libvirtd"
+        "kvm"
+      ];
+      homeMode = "0755";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAywaYwPN4LVbPqkc+kUc7ZVazPBDy4LCAud5iGJdr7g9CwLYoudNjXt/98Oam5lK7ai6QPItK6ECj5+33x/iFpWb3Urr9SqMc/tH5dU1b9N/9yWRhE2WnfcvuI0ms6AXma8QGp1pj/DoLryPVQgXvQlglHaDIL1qdRWFqXUO2u30X5tWtDdOoR02UyAtYBttou4K0rG7LF9rRaoLYP9iCBLxkMJbCIznPD/pIYa6Fl8V8/OVsxYiFy7l5U0RZ7gkzJv8iNz+GG8vw2NX4oIJfAR4oIk3INUvYrKvI2NSMSw5sry+z818fD1hK+soYLQ4VZ4hHRHcf4WV4EeVa5ARxdw== Martin Wimpress"
+      ];
+      packages = [ pkgs.home-manager ];
+      shell = pkgs.fish;
+    };
   };
 }
