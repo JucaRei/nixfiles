1bfb9746a6d229ce56adef5287eaecd593d153ca
Reinaldo <reinaldo800@gmail.com>
2024-07-24 21:30:31 -0300

    fix

--- nixos/_mixins/sys/default.nix
+++ nixos/_mixins/sys/default.nix
@@ -1,11 +1,22 @@
-_: {
-  imports = [
-    ./ananicy.nix
-    ./fwupd.nix
-    # ./check-updates.nix
-    ./irqbalance.nix
-    ./runners.nix
-    ./psd.nix
-    ./thermald.nix
-  ];
+# _: {
+#   imports = [
+#     ./ananicy.nix
+#     ./fwupd.nix
+#     # ./check-updates.nix
+#     ./irqbalance.nix
+#     ./runners.nix
+#     ./psd.nix
+#     ./thermald.nix
+#   ];
+# }
+
+{ lib, ... }:
+let
+  currentDir = ./.; # Represents the current directory
+  isDirectoryAndNotTemplate = name: type: type == "directory";
+  directories = lib.filterAttrs isDirectoryAndNotTemplate (builtins.readDir currentDir);
+  importDirectory = name: import (currentDir + "/${name}");
+in
+{
+  imports = lib.mapAttrsToList (name: _: importDirectory name) directories;
 }
