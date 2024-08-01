8b8dbc476e96a23c1f7ab00228ad10524c914d18
Reinaldo <reinaldo800@gmail.com>
2024-08-01 17:04:07 -0300

    x

--- lib/helpers.nix
+++ lib/helpers.nix
@@ -27,7 +27,7 @@ in
       isInstall = !isISO;
       isLima = builtins.substring 0 5 hostname == "lima-";
       isWorkstation = builtins.isString desktop;
-      nixGL = (import ./nixGL.nix { inherit config pkgs; });
+      nixGL = (import ./nixGL.nix { inherit config; });
     in
     inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = inputs.nixpkgs.legacyPackages.${platform};
