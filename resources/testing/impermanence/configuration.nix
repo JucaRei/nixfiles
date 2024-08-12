7b394298e6099393264e061a5fd84b2ef18d4664
Reinaldo <reinaldo800@gmail.com>
2024-08-12 13:42:42 -0300

    fix

--- resources/testing/impermanence/configuration.nix
+++ resources/testing/impermanence/configuration.nix
@@ -6,16 +6,71 @@
       ./hardware-configuration.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      generationsDir.copyKernels = true;
+    };
+  };
 
-  services.openssh.enable = true;
+  services = {
+    openssh.enable = true;
+    xserver = {
+      enable = true;
+      displayManager = {
+        lightdm = {
+          enable = true;
+          greeters.gtk = {
+            enable = true;
+            cursorTheme.name = "Yaru";
+            cursorTheme.package = pkgs.yaru-theme;
+            cursorTheme.size = 24;
+            iconTheme.name = lib.mkDefault "Yaru-magenta-dark";
+            iconTheme.package = pkgs.yaru-theme;
+            theme.name = lib.mkDefault "Yaru-magenta-dark";
+            theme.package = pkgs.yaru-theme;
+            indicators = [
+              "~session"
+              "~host"
+              "~spacer"
+              "~clock"
+              "~spacer"
+              "~a11y"
+              "~power"
+            ];
+            # https://github.com/Xubuntu/lightdm-gtk-greeter/blob/master/data/lightdm-gtk-greeter.conf
+            extraConfig = ''
+              # background = Background file to use, either an image path or a color (e.g. #772953)
+              font-name = Work Sans 12
+              xft-antialias = true
+              xft-dpi = 96
+              xft-hintstyle = slight
+              xft-rgba = rgb
+
+              active-monitor = #cursor
+              # position = x y ("50% 50%" by default)  Login window position
+              # default-user-image = Image used as default user icon, path or #icon-name
+              hide-user-image = false
+              round-user-image = false
+              highlight-logged-user = true
+              panel-position = top
+              clock-format = %a, %b %d  %H:%M
+            '';
+          };
+        };
+      };
+      desktopManager = { xfce.enable = true; };
+    };
+  };
+
+  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
 
   system.stateVersion = "24.05"; # Did you read the comment?
 
   users.users."juca" = {
     isNormalUser = true;
-    initialPassword = "1";
+    initialPassword = "123";
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
 
@@ -69,5 +124,4 @@
       "vimjoyer" = import ./home.nix;
     };
   };
-
 }
