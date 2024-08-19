4736a6e010ba232b13629c7bff0c3daf6a17b6eb
Reinaldo <reinaldo800@gmail.com>
2024-08-19 18:12:05 -0300

    fix

--- home/default.nix
+++ home/default.nix
@@ -100,7 +100,7 @@ in
     # package = pkgs.nix;
     # auto upgrade nix to the unstable version
     # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/tools/package-management/nix/default.nix#L284
-    nix.package = pkgs.nixVersions.latest;
+    package = pkgs.nixVersions.latest;
 
     settings = {
       auto-optimise-store = isLinux;
