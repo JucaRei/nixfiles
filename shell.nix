044105ded80ac7a8182501194ee4214efb393122
Reinaldo <reinaldo800@gmail.com>
2024-08-22 02:35:37 +0000

    fix env

--- shell.nix
+++ shell.nix
@@ -5,20 +5,24 @@
 {
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
-    NIX_CONFIG = "experimental-features = nix-command flakes ca-derivations";
+    NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [
       git
       home-manager # manage dots
       duf # check space
       nix-output-monitor # better output from builds
       nix-direnv # A shell extension that manages your environment for nix
+      git
+      nil
+      direnv
+      nixpkgs-fmt
       # dropbear # ssh
       figlet
       lolcat
     ];
 
     shellHook = ''
-      $SHELL
+      #$SHELL
       # alias ssh="dbclient"
       echo "Flakes!" | figlet -W | lolcat -F 0.3 -p 2.5 -S 300
     '';
