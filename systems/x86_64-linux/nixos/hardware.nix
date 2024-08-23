d7a94501697ad97774e757614ad2176ce20b982b
JucaRei <32142926+JucaRei@users.noreply.github.com>
2024-08-23 18:21:56 -0300

    Update hardware.nix

--- systems/x86_64-linux/nixos/hardware.nix
+++ systems/x86_64-linux/nixos/hardware.nix
@@ -35,17 +35,79 @@
     extraModulePackages = [ ];
   };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
+  fileSystems =
+    let
+      BTRFS_OPTS = [
+        "noatime"
+        "nodiratime"
+        "nodatacow"
+        "ssd"
+        "compress-force=zstd:15"
+        "space_cache=v2"
+        "commit=120"
+        "discard=async"
+      ];
+    in
+    {
+      "/" = {
+        device = "/dev/disk/by-partlabel/disk-vda-NixOS";
+        # device = "/dev/disk/by-uuid/e9cd822d-be82-4f8d-9f05-b594889110a9";
+        fsType = "btrfs";
+        options = [
+          "subvol=@"
+          "x-gvfs-hide" # hide from filemanager
+        ] ++ BTRFS_OPTS;
+      };
 
-  fileSystems."/boot/efi" = {
-    device = "/dev/disk/by-label/BOOT";
-    fsType = "vfat";
-  };
+      "/home" = {
+        device = "/dev/disk/by-partlabel/disk-vda-NixOS";
+        # device = "/dev/disk/by-uuid/e9cd822d-be82-4f8d-9f05-b594889110a9";
+        fsType = "btrfs";
+        options = [ "subvol=@home" ] ++ BTRFS_OPTS;
+      };
 
-  swapDevices = [ ];
+      "/.snapshots" = {
+        device = "/dev/disk/by-partlabel/disk-vda-NixOS";
+        # device = "/dev/disk/by-uuid/e9cd822d-be82-4f8d-9f05-b594889110a9";
+        fsType = "btrfs";
+        options = [ "subvol=@snapshots" ] ++ BTRFS_OPTS;
+      };
+
+      "/var" = {
+        device = "/dev/disk/by-partlabel/disk-vda-NixOS";
+        fsType = "btrfs";
+        options = [ "subvol=@var" ] ++ BTRFS_OPTS;
+      };
+
+      "/nix" = {
+        device = "/dev/disk/by-partlabel/disk-vda-NixOS";
+        # device = "/dev/disk/by-uuid/e9cd822d-be82-4f8d-9f05-b594889110a9";
+        fsType = "btrfs";
+        options = [ "subvol=@nix" ] ++ BTRFS_OPTS;
+      };
+
+      "/boot" = {
+        device = "/dev/disk/by-partlabel/ESP";
+        fsType = "vfat";
+        options = [
+          "fmask=0022"
+          "dmask=0022"
+          "defaults"
+          "noatime"
+          "nodiratime"
+          "x-gvfs-hide" # hide from filemanager
+        ];
+      };
+    };
+
+  swapDevices = [
+    {
+      # device = "/var/swap/swapfile";
+      # device = "/dev/disk/by-label/swap";
+      device = "/dev/disk/by-partlabel/disk-vda-SWAP";
+      # size = "20G";
+    }
+  ];
 
   hardware = {
     bluetooth.enable = true;
