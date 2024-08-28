65f02260b936f3537126665c1b63a3af45b8b90c
Reinaldo <reinaldo800@gmail.com>
2024-08-28 20:15:47 -0300

    a

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -132,9 +132,9 @@ in
         '';
         # PubkeyAuthentication = "yes";
         # StreamLocalBindUnlink = "yes";
-        UseDns = false;
-        UsePAM = true;
-        X11Forwarding = false;
+        # UseDns = false;
+        # UsePAM = true;
+        # X11Forwarding = false;
 
         # key exchange algorithms recommended by `nixpkgs#ssh-audit`
         # KexAlgorithms = [
