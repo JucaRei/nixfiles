9fe51b1634e1f659f358613db354ac9f628363c0
Reinaldo <reinaldo800@gmail.com>
2024-08-28 15:43:22 -0300

    f

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -210,52 +210,52 @@ in
             (builtins.attrNames other-hosts);
         };
       };
-
-      programs.ssh = {
-        compression = true;
-        forwardAgent = true;
-        serverAliveCountMax = 2;
-        serverAliveInterval = 300;
-        extraOptionOverrides = { Include = "local.d/*"; };
-        extraConfig =
-          ''
-            AddKeysToAgent yes
-          ''
-          + lib.optionalString pkgs.stdenv.isDarwin ''
-            IgnoreUnknown UseKeychain
-            UseKeychain yes
-          '';
-        matchBlocks = {
-          # "github.com" = {
-          #   user = "Reinaldo";
-          #   identityFile = with config.home; "${homeDirectory}/.ssh/github";
-          #   # identityFile = "~/.ssh/github_ed25519";
-          #   # identitiesOnly = true;
-          # };
-          # "git.sr.ht" = {
-          #   identityFile = with config.home; "${homeDirectory}/.ssh/sourcehut";
-          # };
-          # "gitlab.com" = {
-          #   user = "Reinaldo";
-          #   identityFile = with config.home; "${homeDirectory}/.ssh/gitlab";
-          # };
-          "127.*.*.* 192.168.*.* 10.*.*.* 172.16.*.* 172.17.*.* 172.18.*.* 172.19.*.* 172.2?.*.* 172.30.*.* 172.31.*.*" = {
-            extraOptions = {
-              Stricthostkeychecking = "no";
-              Userknownhostsfile = "/dev/null";
-              LogLevel = "quiet";
-            };
-          };
-
-          "*.lan" = {
-            extraOptions = {
-              Stricthostkeychecking = "no";
-              Userknownhostsfile = "/dev/null";
-              LogLevel = "quiet";
-            };
-          };
-        };
-      };
     };
   };
 }
+
+# programs.ssh = {
+#         compression = true;
+#         forwardAgent = true;
+#         serverAliveCountMax = 2;
+#         serverAliveInterval = 300;
+#         extraOptionOverrides = { Include = "local.d/*"; };
+#         extraConfig =
+#           ''
+#             AddKeysToAgent yes
+#           ''
+#           + lib.optionalString pkgs.stdenv.isDarwin ''
+#             IgnoreUnknown UseKeychain
+#             UseKeychain yes
+#           '';
+#         matchBlocks = {
+#           # "github.com" = {
+#           #   user = "Reinaldo";
+#           #   identityFile = with config.home; "${homeDirectory}/.ssh/github";
+#           #   # identityFile = "~/.ssh/github_ed25519";
+#           #   # identitiesOnly = true;
+#           # };
+#           # "git.sr.ht" = {
+#           #   identityFile = with config.home; "${homeDirectory}/.ssh/sourcehut";
+#           # };
+#           # "gitlab.com" = {
+#           #   user = "Reinaldo";
+#           #   identityFile = with config.home; "${homeDirectory}/.ssh/gitlab";
+#           # };
+#           "127.*.*.* 192.168.*.* 10.*.*.* 172.16.*.* 172.17.*.* 172.18.*.* 172.19.*.* 172.2?.*.* 172.30.*.* 172.31.*.*" = {
+#             extraOptions = {
+#               Stricthostkeychecking = "no";
+#               Userknownhostsfile = "/dev/null";
+#               LogLevel = "quiet";
+#             };
+#           };
+
+#           "*.lan" = {
+#             extraOptions = {
+#               Stricthostkeychecking = "no";
+#               Userknownhostsfile = "/dev/null";
+#               LogLevel = "quiet";
+#             };
+#           };
+#         };
+#       };
