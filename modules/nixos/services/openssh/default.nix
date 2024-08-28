5e7ad9db0bb4d7e916bcce5253054c418c446bf5
Reinaldo <reinaldo800@gmail.com>
2024-08-28 20:12:36 -0300

    rm

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -137,21 +137,21 @@ in
         X11Forwarding = false;
 
         # key exchange algorithms recommended by `nixpkgs#ssh-audit`
-        KexAlgorithms = [
-          "curve25519-sha256"
-          "curve25519-sha256@libssh.org"
-          "diffie-hellman-group16-sha512"
-          "diffie-hellman-group18-sha512"
-          "diffie-hellman-group-exchange-sha256"
-          "sntrup761x25519-sha512@openssh.com"
-        ];
+        # KexAlgorithms = [
+        #   "curve25519-sha256"
+        #   "curve25519-sha256@libssh.org"
+        #   "diffie-hellman-group16-sha512"
+        #   "diffie-hellman-group18-sha512"
+        #   "diffie-hellman-group-exchange-sha256"
+        #   "sntrup761x25519-sha512@openssh.com"
+        # ];
 
         # message authentication code algorithms recommended by `nixpkgs#ssh-audit`
-        Macs = [
-          "hmac-sha2-512-etm@openssh.com"
-          "hmac-sha2-256-etm@openssh.com"
-          "umac-128-etm@openssh.com"
-        ];
+        # Macs = [
+        #   "hmac-sha2-512-etm@openssh.com"
+        #   "hmac-sha2-256-etm@openssh.com"
+        #   "umac-128-etm@openssh.com"
+        # ];
       };
 
       startWhenNeeded = true;
