d8978dc8939eabe04cb8088d19fecab12c0eb2c0
Reinaldo <reinaldo800@gmail.com>
2024-08-29 12:43:45 -0300

    check

--- flake.nix
+++ flake.nix
@@ -273,6 +273,8 @@
 
       deploy = lib.mkDeploy { inherit (inputs) self; };
 
-      outputs-builder = channels: { formatter = channels.nixpkgs.nixfmt-rfc-style; };
+      outputs-builder = channels: {
+        formatter = channels.nixpkgs.nixfmt-rfc-style;
+      };
     };
 }
