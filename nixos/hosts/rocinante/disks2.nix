4d02d1e146083db0527a2566b37871550eb24fed
Reinaldo P Jr <reinaldo800@gmail.com>
2024-10-02 17:54:58 -0300

    testing

--- nixos/hosts/rocinante/disks2.nix
+++ nixos/hosts/rocinante/disks2.nix
@@ -4,101 +4,101 @@ let
   defaultExt4Opts = [ "defaults" "noatime" "nodiratime" "commit=60" ];
 in
 {
-  disko.devices = {
-    disk = {
-      sda = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-SanDisk_SSD_PLUS_120_GB_181102802196";
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "boot";
-              start = "0";
-              end = "1M";
-              part-type = "primary";
-              flags = [ "bios_grub" ];
-            }
-            {
-              name = "ESP";
-              start = "1M";
-              end = "550MiB";
-              bootable = true;
-              flags = [ "esp" ];
-              fs-type = "fat32";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            }
-            # {
-            #   name = "nixswap";
-            #   start = "-6GiB";
-            #   end = "100%";
-            #   fs-type = "linux-swap";
-            #   content = {
-            #     type = "swap";
-            #   };
-            # }
-            {
-              name = "swap";
-              size = "6G";
-              content = {
-                type = "swap";
-                resumeDevice = true;
-              };
-            }
-            {
-              name = "nixsystem";
-              start = "550MiB";
-              # end = "-6GiB";
-              end = "100%";
-              content = {
-                type = "filesystem";
-                extraArgs = [ "-f" ];
-                format = "ext4";
-                mountpoint = "/";
-                mountOptions = defaultExt4Opts;
-              };
-            }
+  # disko.devices = {
+  #   disk = {
+  #     sda = {
+  #       type = "disk";
+  #       device = "/dev/disk/by-id/ata-SanDisk_SSD_PLUS_120_GB_181102802196";
+  #       content = {
+  #         type = "table";
+  #         format = "gpt";
+  #         partitions = [
+  #           {
+  #             name = "boot";
+  #             start = "0";
+  #             end = "1M";
+  #             part-type = "primary";
+  #             flags = [ "bios_grub" ];
+  #           }
+  #           {
+  #             name = "ESP";
+  #             start = "1M";
+  #             end = "550MiB";
+  #             bootable = true;
+  #             flags = [ "esp" ];
+  #             fs-type = "fat32";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "vfat";
+  #               mountpoint = "/boot";
+  #             };
+  #           }
+  #           # {
+  #           #   name = "nixswap";
+  #           #   start = "-6GiB";
+  #           #   end = "100%";
+  #           #   fs-type = "linux-swap";
+  #           #   content = {
+  #           #     type = "swap";
+  #           #   };
+  #           # }
+  #           {
+  #             name = "swap";
+  #             size = "6G";
+  #             content = {
+  #               type = "swap";
+  #               resumeDevice = true;
+  #             };
+  #           }
+  #           {
+  #             name = "nixsystem";
+  #             start = "550MiB";
+  #             # end = "-6GiB";
+  #             end = "100%";
+  #             content = {
+  #               type = "filesystem";
+  #               extraArgs = [ "-f" ];
+  #               format = "ext4";
+  #               mountpoint = "/";
+  #               mountOptions = defaultExt4Opts;
+  #             };
+  #           }
 
-            #   # {
-            #   #   name = "root";
-            #   #   start = "512MiB";
-            #   #   end = "100%";
-            #   #   content = {
-            #   #     type = "btrfs";
-            #   #     extraArgs = [ "-f" ];
-            #   #     subvolumes = {
-            #   #       "/rootfs" = {
-            #   #         mountpoint = "/";
-            #   #       };
-            #   #       # Mountpoints inferred from subvolume name
-            #   #       "/home" = {
-            #   #         mountOptions = [ "compress=zstd" ];
-            #   #       };
-            #   #       "/nix" = {
-            #   #         mountOptions = [
-            #   #           "compress=zstd"
-            #   #           "noatime"
-            #   #         ];
-            #   #       };
-            #   #       # "/persist" = {
-            #   #       #   mountOptions = [
-            #   #       #     "compress=zstd"
-            #   #       #     "noatime"
-            #   #       #   ];
-            #   #       # };
-            #   #     };
-            #   #   };
-            #   # }
-          ];
-        };
-      };
-    };
-  };
+  #           #   # {
+  #           #   #   name = "root";
+  #           #   #   start = "512MiB";
+  #           #   #   end = "100%";
+  #           #   #   content = {
+  #           #   #     type = "btrfs";
+  #           #   #     extraArgs = [ "-f" ];
+  #           #   #     subvolumes = {
+  #           #   #       "/rootfs" = {
+  #           #   #         mountpoint = "/";
+  #           #   #       };
+  #           #   #       # Mountpoints inferred from subvolume name
+  #           #   #       "/home" = {
+  #           #   #         mountOptions = [ "compress=zstd" ];
+  #           #   #       };
+  #           #   #       "/nix" = {
+  #           #   #         mountOptions = [
+  #           #   #           "compress=zstd"
+  #           #   #           "noatime"
+  #           #   #         ];
+  #           #   #       };
+  #           #   #       # "/persist" = {
+  #           #   #       #   mountOptions = [
+  #           #   #       #     "compress=zstd"
+  #           #   #       #     "noatime"
+  #           #   #       #   ];
+  #           #   #       # };
+  #           #   #     };
+  #           #   #   };
+  #           #   # }
+  #         ];
+  #       };
+  #     };
+  #   };
+  # };
   # nodev = {
   #   "/tmp" = {
   #     fsType = "tmpfs";
@@ -109,4 +109,66 @@ in
   #     ];
   #   };
   # };
+
+  disko.devices = {
+    disk = {
+      sda = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-SanDisk_SSD_PLUS_120_GB_181102802196";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = {
+            boot = {
+              start = "0";
+              end = "1M";
+              part-type = "primary";
+              flags = [ "bios_grub" ];
+            };
+            ESP = {
+              start = "1M";
+              end = "550MiB";
+              bootable = true;
+              flags = [ "esp" ];
+              fs-type = "fat32";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            nixroot = {
+              start = "550MiB";
+              end = "-6GiB";
+              # end = "100%";
+              content = {
+                type = "filesystem";
+                extraArgs = [ "-f" ];
+                format = "ext4";
+                mountpoint = "/";
+                mountOptions = defaultExt4Opts;
+              };
+            };
+            nixswap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+          };
+        };
+      };
+      #   };
+      # };
+      # nodev = {
+      #   "/tmp" = {
+      #     fsType = "tmpfs";
+      #     mountOptions = [
+      #       "defaults"
+      #       "size=4G"
+      #       "mode=755"
+      #     ];
+    };
+  };
 }
