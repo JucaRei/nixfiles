27653133a3efd36621dc91bc48b0ab1186158064
Reinaldo <reinaldo800@gmail.com>
2024-10-07 23:08:40 +0000

    fix import

--- nixos/default.nix
+++ nixos/default.nix
@@ -182,7 +182,7 @@ in
           nano = "micro";
         };
       };
-      nano.enable = import ../resources/nixos/scripts/nano.nix { inherit pkgs config lib; };
+      nano = import ../resources/nixos/scripts/nano.nix { inherit pkgs config lib; };
       nh = {
         clean = {
           enable = true;
