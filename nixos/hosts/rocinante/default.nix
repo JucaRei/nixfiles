9b5bf41c9609885bcd4f645f481c14a056792991
Reinaldo <reinaldo800@gmail.com>
2024-10-08 00:52:36 +0000

    fixed rocinante b43 driver

--- nixos/hosts/rocinante/default.nix
+++ nixos/hosts/rocinante/default.nix
@@ -29,13 +29,9 @@ in
     };
 
     hardware = {
-      enableAllFirmware = true;
-      # firmware = [ pkgs.b43Firmware_5_1_138 ];
-
-      # opengl = {
-      #   driSupport = true;
-      #   driSupport32Bit = true;
-      # };
+      firmware = [
+        pkgs.b43Firmware_6_30_163_46
+      ];
     };
 
     boot = {
@@ -51,44 +47,26 @@ in
           "sd_mod"
           "sr_mod"
         ];
-        kernelModules = [
-          "wl" # load broadcom wireless driver
-          # "b43"
-          # "bcm5974"
-        ];
       };
       # blacklist similar modules to avoid collision
       blacklistedKernelModules = [
-        "b43"
         "bcma"
         "ssb"
         "brcmfmac"
         "brcmsmac"
         "bcma"
-        # "wl"
+        "wl"
       ];
       kernelModules = [
         # "kvm-intel"
         # "applesmc"
         "b43"
-        # "wl" # set of kernel modules loaded in second stage of boot process
         "bcm5974" # touchpad
       ];
 
       extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
-      # extraModulePackages = [
-      #   (config.boot.kernelPackages.broadcom_sta.overrideAttrs (old: {
-      #     patches = old.patches ++ [
-      #       (builtins.fetchurl {
-      #         url = "https://raw.githubusercontent.com/archlinux/svntogit-community/5ec5b248976f84fcd7e3d7fae49ee91289912d12/trunk/012-linux517.patch";
-      #         sha256 = "df557afdb0934ed2de6ab967a350d777cbb7b53bf0b1bdaaa7f77a53102f30ac";
-      #       })
-      #     ];
-      #   }))
-      # ];
 
       kernelPackages = mkForce pkgs.linuxPackages_6_6;
-      #kernelParams = [ "intel_idle.max_cstate=1" "hid_apple.iso_layout=0" "acpi_backlight=vendor" "acpi_mask_gpe=0x15" ];
       kernelParams = [
         "intel_idle.max_cstate=1"
         "acpi_backlight=vendor"
@@ -101,9 +79,6 @@ in
         # theme = pkgs.cyberre-grub-theme;
         device = "/dev/sda";
       };
-      #extraModprobeConfig = ''
-      #  options bcm5974
-      #'';
     };
 
     services = {
@@ -143,7 +118,7 @@ in
       inxi
 
       cachix
-      icloudpd # iCloud Photos Downloader
+      icloudpd # iCloud Photos Downloader,
     ];
 
     networking.enableB43Firmware = true;
