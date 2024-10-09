1a19c2dbb9e3484e9de6deba716a17b1e2e0dcd2
Reinaldo P Jr <reinaldo800@gmail.com>
2024-10-09 10:51:45 -0300

    fix

--- nixos/_mixins/desktop/features/flatpak-appcenter/default.nix
+++ nixos/_mixins/desktop/features/flatpak-appcenter/default.nix
@@ -14,20 +14,22 @@ in
     };
   };
 
-  services = {
-    flatpak = {
-      enable = true;
-      # By default nix-flatpak will add the flathub remote;
-      # Therefore Appcenter is only added when the desktop is Pantheon
-      remotes = lib.mkIf (desktop == "pantheon") [
-        {
-          name = "appcenter";
-          location = "https://flatpak.elementary.io/repo.flatpakrepo";
-        }
-      ];
-      update.auto = {
+  config = mkIf cfg.enable {
+    services = {
+      flatpak = {
         enable = true;
-        onCalendar = "weekly";
+        # By default nix-flatpak will add the flathub remote;
+        # Therefore Appcenter is only added when the desktop is Pantheon
+        remotes = lib.mkIf (desktop == "pantheon") [
+          {
+            name = "appcenter";
+            location = "https://flatpak.elementary.io/repo.flatpakrepo";
+          }
+        ];
+        update.auto = {
+          enable = true;
+          onCalendar = "weekly";
+        };
       };
     };
   };
