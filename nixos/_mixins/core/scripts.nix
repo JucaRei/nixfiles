01bec363b069e333b14a74f5d66ac124015af075
Reinaldo <reinaldo800@gmail.com>
2024-12-14 14:41:50 -0300

    fix path

--- nixos/_mixins/core/scripts.nix
+++ nixos/_mixins/core/scripts.nix
@@ -1,12 +1,12 @@
 { hostname, lib, pkgs, isInstall, config, ... }:
 let
-  build-all = import ../../../resources/nixos/scripts2/build-all.nix { inherit pkgs; };
-  build-host = import ../../../resources/nixos/scripts2/build-host.nix { inherit pkgs; };
-  build-iso = import ../../../resources/nixos/scripts2/build-iso.nix { inherit pkgs; };
-  flatpak-theme = import ../../../resources/nixos/scripts2/flatpak-theme.nix { inherit pkgs; };
-  switch-all = import ../../../resources/nixos/scripts2/switch-all.nix { inherit pkgs; };
-  switch-host = import ../../../resources/nixos/scripts2/switch-host.nix { inherit pkgs; };
-  switch-boot = import ../../../resources/nixos/scripts2/switch-boot.nix { inherit pkgs; };
+  build-all = import ../../../resources/nixos/scripts/build-all.nix { inherit pkgs; };
+  build-host = import ../../../resources/nixos/scripts/build-host.nix { inherit pkgs; };
+  build-iso = import ../../../resources/nixos/scripts/build-iso.nix { inherit pkgs; };
+  flatpak-theme = import ../../../resources/nixos/scripts/flatpak-theme.nix { inherit pkgs; };
+  switch-all = import ../../../resources/nixos/scripts/switch-all.nix { inherit pkgs; };
+  switch-host = import ../../../resources/nixos/scripts/switch-host.nix { inherit pkgs; };
+  switch-boot = import ../../../resources/nixos/scripts/switch-boot.nix { inherit pkgs; };
 in
 {
   imports = [
