24d4ac50c722abc3311bb3ee5f849f22b0fd7e84
Reinaldo <reinaldo800@gmail.com>
2025-01-21 17:01:49 -0300

    fix

--- pkgs/fonts/font-search/default.nix
+++ pkgs/fonts/font-search/default.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, ... }:
 pkgs.writeShellScriptBin "font-search" ''
   #!/usr/bin/env bash
-  ${lib.getExe' pkgs.ontconfig "fc-list"} \
+  ${lib.getExe' pkgs.fontconfig "fc-list"} \
     | ${lib.getExe' pkgs.gnugrep "grep"} -ioE ": [^:]*$1[^:]+:" \
     | ${pkgs.gnused}/bin/sed -E 's/(^: |:)//g' \
     | ${lib.getExe' pkgs.uutils-coreutils-noprefix "tr"} , \\n \
