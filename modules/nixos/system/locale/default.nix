44e25981ff3de12b805423799f379006dc5c88f6
Reinaldo <reinaldo800@gmail.com>
2025-02-05 16:19:03 -0300

    add cpu type

--- modules/nixos/system/locale/default.nix
+++ modules/nixos/system/locale/default.nix
@@ -1,11 +1,4 @@
-{
-  options,
-  config,
-  pkgs,
-  lib,
-  namespace,
-  ...
-}:
+{ config, lib, namespace, ... }:
 with lib;
 with lib.${namespace};
 let
@@ -17,7 +10,26 @@ in
   };
 
   config = mkIf cfg.enable {
-    i18n.defaultLocale = "en_US.UTF-8";
+    i18n = {
+      defaultLocale = "en_US.utf8";
+      extraLocaleSettings = {
+        #LC_CTYPE =  "pt_BR.UTF-8"; # Fix ç in us-intl.
+        LC_ADDRESS = "pt_BR.UTF-8";
+        LC_IDENTIFICATION = "pt_BR.UTF-8";
+        LC_MEASUREMENT = "pt_BR.UTF-8";
+        LC_MONETARY = "pt_BR.UTF-8";
+        LC_NAME = "pt_BR.UTF-8";
+        LC_NUMERIC = "pt_BR.UTF-8";
+        LC_PAPER = "pt_BR.UTF-8";
+        LC_TELEPHONE = "pt_BR.UTF-8";
+        LC_TIME = "pt_BR.UTF-8";
+      };
+    };
+
+    supportedLocales = [
+      "pt_BR.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+    ];
 
     console = {
       keyMap = mkForce "us";
