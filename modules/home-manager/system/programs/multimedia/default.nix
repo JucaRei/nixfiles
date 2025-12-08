c96e64d59e3284c4f2e7d75d90c03848a0a8ff5c
Reinaldo <reinaldo800@gmail.com>
2025-12-08 22:44:57 +0000

    Add enable options to mpv and rhythmbox submodules

--- modules/home-manager/system/programs/multimedia/default.nix
+++ modules/home-manager/system/programs/multimedia/default.nix
@@ -4,12 +4,20 @@
     type = lib.types.submodule {
       options = {
         mpv = lib.mkOption {
-          type = lib.types.submodule { };
+          type = lib.types.submodule {
+            options = {
+              enable = lib.mkEnableOption "mpv media player";
+            };
+          };
           default = { };
           description = "MPV player configuration";
         };
         rhythmbox = lib.mkOption {
-          type = lib.types.submodule { };
+          type = lib.types.submodule {
+            options = {
+              enable = lib.mkEnableOption "rhythmbox music player";
+            };
+          };
           default = { };
           description = "Rhythmbox music player configuration";
         };
