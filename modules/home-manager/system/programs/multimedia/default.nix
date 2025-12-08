9c83ddd58039652748cabda9ac875f31c5d0d339
Reinaldo <reinaldo800@gmail.com>
2025-12-08 22:42:31 +0000

    Add parent option definition for system.programs.multimedia

--- modules/home-manager/system/programs/multimedia/default.nix
+++ modules/home-manager/system/programs/multimedia/default.nix
@@ -1,4 +1,10 @@
-_: {
+{ lib, ... }:
+{
+  options.system.programs.multimedia = lib.mkOption {
+    type = lib.types.submodule { };
+    description = "Multimedia programs configuration";
+  };
+
   imports = [
     ./mpv
     ./rhythmbox
