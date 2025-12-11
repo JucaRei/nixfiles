e44a52b19d1b372d8a505ca2edc77c1f9ec565c7
Reinaldo <reinaldo800@gmail.com>
2025-12-11 05:02:55 +0000

    add vscode server hm

--- home-manager/hosts/vm/default.nix
+++ home-manager/hosts/vm/default.nix
@@ -1,11 +1,24 @@
 { pkgs, ... }: {
-  home.packages = [ pkgs.vscode ];
+  imports = [
+    # Import the vscode-server home-manager module
+    "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+  ];
 
-  programs.vscode = {
+  # Enable the service
+  services.vscode-server = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      ms-vscode-remote.remote-ssh
-      ms-vscode-remote.remote-containers
-    ];
+    enableFHS = true;
   };
+
+  # Optional: Enable an FHS environment for better extension compatibility
+
+  # home.packages = [ pkgs.vscode ];
+
+  # programs.vscode = {
+  #   enable = true;
+  #   extensions = with pkgs.vscode-extensions; [
+  #     ms-vscode-remote.remote-ssh
+  #     ms-vscode-remote.remote-containers
+  #   ];
+  # };
 }
