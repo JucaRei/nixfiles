0eb33dce03e86abb1f35ba681be9b026ac2431f8
Reinaldo <reinaldo800@gmail.com>
2025-12-18 15:05:03 -0300

    test

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -24,9 +24,9 @@ in
           after = [ "writeBoundary" "createXdgUserDirectories" ];
           before = [ ];
           data = ''
-            rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+            rm -rf ${config.home.homeDirectory}/.local/share/applications
             rm -rf ${config.home.homeDirectory}/.icons/nix-icons
-            mkdir -p ${config.home.homeDirectory}/.local/share/applications/home-manager
+            mkdir -p ${config.home.homeDirectory}/.local/share/applications
             mkdir -p ${config.home.homeDirectory}/.icons
             ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.icons/nix-icons
 
@@ -40,7 +40,7 @@ in
             # Symlink new desktop entries
             for desktop_file in ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop; do
               if ! echo "$current_files" | grep -q "$(basename $desktop_file)"; then
-                ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/home-manager/$(basename $desktop_file)
+                ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/$(basename $desktop_file)
               fi
             done
 
@@ -60,7 +60,7 @@ in
     };
 
     programs.bash.profileExtra = lib.mkAfter ''
-      rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+      rm -rf ${config.home.homeDirectory}/.local/share/applications
       rm -rf ${config.home.homeDirectory}/.icons/nix-icons
       ls ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop > ${config.home.homeDirectory}/.cache/current_desktop_files.txt
     '';
