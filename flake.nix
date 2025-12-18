a0165db2f46e4cef30a1e3c7a7bf99db831a9866
Reinaldo <reinaldo800@gmail.com>
2025-12-18 20:22:31 -0300

    set hm to 25.05 version

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
           hostname = "virtualvm";
           desktop = "xfce4";
           useNixGL = true;
-          stateVersion = "25.11";
+          stateVersion = "25.05";
         };
       };
       nixosConfigurations = {
