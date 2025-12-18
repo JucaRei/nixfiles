a6a32068aaa2c07ee3319840491a0c255f4f92ce
Reinaldo <reinaldo800@gmail.com>
2025-12-18 15:01:07 -0300

    feat(desktop): enhance desktop apps linking for non-NixOS systems
    - Fix typo in activation script name: "linkDestopApplications" to "linkDesktopApplications"
    - Expand activation script to symlink icons, cache desktop files, and only link new entries to avoid redundancy
    - Add bash profile extra to clean up directories and update cache on login
    - Improves performance and prevents unnecessary desktop database updates on each activation

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -19,11 +19,34 @@ in
       ]);
 
       activation = {
-        linkDestopApplications = mkIf (!isNixOS) {
+        linkDesktopApplications = mkIf (!isNixOS) {
           # Add Packages To System Menu by updating database
           after = [ "writeBoundary" "createXdgUserDirectories" ];
           before = [ ];
-          data = "${pkgs.desktop-file-utils}/bin/update-desktop-database";
+          data = ''
+            rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+            rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+            mkdir -p ${config.home.homeDirectory}/.local/share/applications/home-manager
+            mkdir -p ${config.home.homeDirectory}/.icons
+            ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.icons/nix-icons
+
+            # Check if the cached desktop files list exists
+            if [ -f ${config.home.homeDirectory}/.cache/current_desktop_files.txt ]; then
+              current_files=$(cat ${config.home.homeDirectory}/.cache/current_desktop_files.txt)
+            else
+              current_files=""
+            fi
+
+            # Symlink new desktop entries
+            for desktop_file in ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop; do
+              if ! echo "$current_files" | grep -q "$(basename $desktop_file)"; then
+                ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/home-manager/$(basename $desktop_file)
+              fi
+            done
+
+            # Update desktop database
+            ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
+          '';
         };
 
         "user-dirs" = lib.hm.dag.entryBefore [ "writeBoundary" ] ''
@@ -35,6 +58,12 @@ in
         "$HOME/.local/bin"
       ];
     };
+
+    programs.bash.profileExtra = lib.mkAfter ''
+      rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+      rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+      ls ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop > ${config.home.homeDirectory}/.cache/current_desktop_files.txt
+    '';
 
     xdg = {
       enable = mkForce true;
