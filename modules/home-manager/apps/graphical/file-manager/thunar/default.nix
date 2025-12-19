50f927e55a0b552746253fba6f7c823540a723b7
Reinaldo <reinaldo800@gmail.com>
2025-12-18 23:55:58 -0300

    ck

--- modules/home-manager/apps/graphical/file-manager/thunar/default.nix
+++ modules/home-manager/apps/graphical/file-manager/thunar/default.nix
@@ -52,59 +52,59 @@ in
         ".config/Thunar/uca.xml".text = ''
           <?xml version="1.0" encoding="UTF-8"?>
           <actions>
-              <action>
-                  <icon>xterm</icon>
-                  <name>Open Terminal Here</name>
-                  <unique-id>1612104464586264-1</unique-id>
-                  <command>${pkgs.xfce.exo}/bin/exo-open --working-directory %f --launch TerminalEmulator</command>
-                  <description>Example for a custom action</description>
-                  <patterns>*</patterns>
-                  <startup-notify/>
-                  <directories/>
-              </action>
-              <action>
-                  <icon>${pkgs.meld}/share/icons/hicolor/symbolic/apps/org.gnome.Meld-symbolic.svg</icon>
-                  <name>Compare</name>
-                  <submenu></submenu>
-                  <unique-id>1622791692322694-4</unique-id>
-                  <command>${pkgs.meld}/bin/meld %F</command>
-                  <description>Compare files and directories with  meld</description>
-                  <range></range>
-                  <patterns>*</patterns>
-                  <directories/>
-                  <text-files/>
-              </action>
-              <action>
-                  <icon>system-file-manager-root</icon>
-                  <name>Open folder as root</name>
-                  <unique-id>1493475601060449-3</unique-id>
-                  <command>${pkgs.polkit}/bin/pkexec ${finalThunar}/bin/thunar %f</command>
-                  <description></description>
-                  <patterns>*</patterns>
-                  <directories/>
-              </action>
-              <action>
-                  <icon>catfish</icon>
-                  <name>Search with catfish</name>
-                  <unique-id>1489089852658523-2</unique-id>
-                  <command>${pkgs.xfce.catfish}/bin/catfish --path=$f$d</command>
-                  <description></description>
-                  <patterns>*</patterns>
-                  <directories/>
-              </action>
-              <action>
-                  <icon>archive-extract</icon>
-                  <name>Extract here</name>
-                  <unique-id>1489091300385082-4</unique-id>
-                  <command>tar xjf %n</command>
-                  <description></description>
-                  <patterns>*.tar.bz2;*.tbz2</patterns>
-                  <other-files/>
-              </action>
-          </actions>
+        '' + ''
+          <action>
+              <icon>xterm</icon>
+              <name>Open Terminal Here</name>
+              <unique-id>1612104464586264-1</unique-id>
+              <command>${pkgs.xfce.exo}/bin/exo-open --working-directory %f --launch TerminalEmulator</command>
+              <description>Example for a custom action</description>
+              <patterns>*</patterns>
+              <startup-notify/>
+              <directories/>
+          </action>
+          <action>
+              <icon>${pkgs.meld}/share/icons/hicolor/symbolic/apps/org.gnome.Meld-symbolic.svg</icon>
+              <name>Compare</name>
+              <submenu></submenu>
+              <unique-id>1622791692322694-4</unique-id>
+              <command>${pkgs.meld}/bin/meld %F</command>
+              <description>Compare files and directories with  meld</description>
+              <range></range>
+              <patterns>*</patterns>
+              <directories/>
+              <text-files/>
+          </action>
+          <action>
+              <icon>system-file-manager-root</icon>
+              <name>Open folder as root</name>
+              <unique-id>1493475601060449-3</unique-id>
+              <command>${pkgs.polkit}/bin/pkexec ${finalThunar}/bin/thunar %f</command>
+              <description></description>
+              <patterns>*</patterns>
+              <directories/>
+          </action>
+          <action>
+              <icon>catfish</icon>
+              <name>Search with catfish</name>
+              <unique-id>1489089852658523-2</unique-id>
+              <command>${pkgs.xfce.catfish}/bin/catfish --path=$f$d</command>
+              <description></description>
+              <patterns>*</patterns>
+              <directories/>
+          </action>
+          <action>
+              <icon>archive-extract</icon>
+              <name>Extract here</name>
+              <unique-id>1489091300385082-4</unique-id>
+              <command>tar xjf %n</command>
+              <description></description>
+              <patterns>*.tar.bz2;*.tbz2</patterns>
+              <other-files/>
+          </action>
         ''
         +
-        lib.optionalString (config.apps.graphical.editor.vscode.enable) ''
+        lib.optionalString (config.apps.graphical.editor.vscode.enable == true) ''
           <action>
               <icon>${config.programs.vscode.package}/share/pixmaps/vscode.png</icon>
               <name>Open VSCode Here</name>
@@ -115,6 +115,8 @@ in
               <startup-notify/>
               <directories/>
           </action>
+        '' + ''
+          </actions>
         '';
         # <command>${lib.getExe config.programs.vscode.package} %f</command>
       };
