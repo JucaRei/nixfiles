69e7ed874c64d0bce9f811e239836036e2a75bbe
Reinaldo <reinaldo800@gmail.com>
2026-02-24 19:51:01 -0300

    check

--- home-manager/hosts/fedora/default.nix
+++ home-manager/hosts/fedora/default.nix
@@ -1,13 +1,19 @@
 { config, ... }: {
-  # config = {
-  #   system = {
-  #     services = {
-  #       git = {
-  #         enable = true;
-  #         user = "Reinaldo";
-  #         email = "reinaldo800@gmail.com";
-  #       };
-  #     };
-  #   };
-  # };
+  config = {
+    system = {
+      # services = {
+      #   git = {
+      #     enable = true;
+      #     user = "Reinaldo";
+      #     email = "reinaldo800@gmail.com";
+      #   };
+      # };
+      programs = {
+        console = {
+          bat.enable = true;
+          eza.enable = true;
+        };
+      };
+    };
+  };
 }
