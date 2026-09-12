6a6b161dc919d576d2edd0c306cae544d588e5c5
Reinaldo <reinaldo800@gmail.com>
2026-09-12 11:57:07 -0300

    feat(eza): add predefined themes support with kanagawa-wave as default

--- modules/home-manager/system/programs/console/eza/default.nix
+++ modules/home-manager/system/programs/console/eza/default.nix
@@ -1,8 +1,33 @@
 { config, lib, ... }:
 let
-  inherit (lib) mkOption mkIf getExe;
-  inherit (lib.types) bool;
+  inherit (lib)
+    mkOption
+    mkIf
+    getExe
+    toLower
+    concatStringsSep
+    ;
+  inherit (lib.types)
+    bool
+    str
+    nullOr
+    either
+    attrsOf
+    anything
+    ;
   cfg = config.system.programs.console.eza;
+
+  predefinedThemes = {
+    "kanagawa-wave" = ./themes/kanagawa-wave.yml;
+    "catppuccin-mocha" = ./themes/catppuccin-mocha.yml;
+    "tokyonight" = ./themes/tokyonight.yml;
+  };
+
+  normalizedTheme =
+    if builtins.isString cfg.theme then
+      toLower (builtins.replaceStrings [ " " "_" ] [ "-" "-" ] cfg.theme)
+    else
+      null;
 in
 {
   options = {
@@ -11,6 +36,15 @@ in
         default = false;
         type = bool;
         description = "Enable's eza.";
+      };
+
+      theme = mkOption {
+        default = "kanagawa-wave";
+        type = nullOr (either str (attrsOf anything));
+        description = ''
+          Theme for eza. Can be a predefined theme name (e.g. "kanagawa-wave" / "Kanagawa Wave",
+          "catppuccin-mocha", "tokyonight") or a custom YAML attrset. Set to null to disable custom theming.
+        '';
       };
     };
   };
@@ -21,6 +55,7 @@ in
       git = true;
       icons = "auto";
       colors = "always";
+      theme = if builtins.isAttrs cfg.theme then cfg.theme else { };
       enableBashIntegration = mkIf (config.system.programs.shells.default == "bash") true;
       enableFishIntegration = mkIf (config.system.programs.shells.default == "fish") true;
       enableZshIntegration = mkIf (config.system.programs.shells.default == "zsh") true;
@@ -33,6 +68,13 @@ in
         "-l"
       ];
     };
+
+    xdg.configFile."eza/theme.yml" = mkIf (normalizedTheme != null && normalizedTheme != "") {
+      source =
+        predefinedThemes.${normalizedTheme}
+          or (throw "Unknown eza theme: '${cfg.theme}'. Available: ${concatStringsSep ", " (builtins.attrNames predefinedThemes)}");
+    };
+
     home = {
       shellAliases = {
         l2 = "${getExe config.programs.eza.package} --color=always --icons --header --time-style=long-iso -l -T -h -L=2";
